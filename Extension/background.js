1888243853f6d28d692a3289860059714282a8cb
Timeraa <29104008+Timeraa@users.noreply.github.com>
2018-07-24 17:01:50 +0200

    Removed redundant code.

--- Extension/background.js
+++ Extension/background.js
@@ -1,40 +1,4 @@
-function updatePresence(tabId, changeInfo, tab) {
-  if (tab.url) {
-    // Create a URL object from the current tab URL
-    const url = new URL(tab.url);
-    // Parse data
-    if (tab.url.startsWith("https://music.youtube.com/")) {
-      const data = {
-        connected: true,
-        service: "ytm",
-        url: tab.url
-      }
-    } else {
-      const data = {
-        connected: true,
-        service: "NaN"
-      }
-    }
-
-    if (!data) return;
-  
-    $.ajax({
-      async: true,
-      crossDomain: true,
-      url: "http://localhost:3000/",
-      method: "POST",
-      headers: {
-        "content-type": "application/json"
-      },
-      processData: false,
-      data: JSON.stringify(data),
-      error: function(jqXHR, textStatus, errorThrown) {}
-    })
-  }
-}
-
-chrome.tabs.onUpdated.addListener(updatePresence);
-
+//* Used to send response to app
 function sendAlive() {
   $.ajax({
     async: true,
