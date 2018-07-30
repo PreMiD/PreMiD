19dcd072c746db65219e7556653d649f0d3d9d5a
Timeraa <29104008+Timeraa@users.noreply.github.com>
2018-07-30 16:58:32 +0200

    V1.0 handleYTM recode

--- Extension/handleYTM.js
+++ Extension/handleYTM.js
@@ -1,58 +1,69 @@
 //* Start interval
-window.onload = function() {
-  setInterval(updateData, 1000);
-};
+$(document).ready(() => {
+  setInterval(updateData, 1000)
+})
 
 //* Create needed variables
-let urlForVideo, songTime, calcDifference;
+let songTime,
+splitTime,
+songStartTime,
+songEndTime,
+songTitle,
+songCover,
+songAuthors = []
 
 function updateData() {
-  if (document.location.href.includes("music.youtube")) urlForVideo = document.location.href;
+  //* Clear old authors
+  songAuthors = []
+
   if($(".time-info.style-scope.ytmusic-player-bar").html() != "" && 
   $(".time-info.style-scope.ytmusic-player-bar").html() != " ") {
+    //* Get song Time String (2:10 / 3:21)
+    songTime = $(".time-info.style-scope.ytmusic-player-bar").html()
+    //* Split to array ["2:10", "3:21"]
+    splitTime = songTime.split(" / ", 2)
+    //* Convert to seconds
+    songStartTime = getSeconds(splitTime[0])
+    songEndTime = getSeconds(splitTime[1])
+    //* Get Song title
+    songTitle = $(".title.style-scope.ytmusic-player-bar").html()
+    //* Get Song cover/thumbnail
+    songCover = $(".image.style-scope.ytmusic-player-bar").attr("src")
 
-    songTime = $(".time-info.style-scope.ytmusic-player-bar").html();
-    calcDifference = songTime.split(" / ", 2);
-
-  var currentSongAuthors = []
-
-  $(".byline.ytmusic-player-bar").contents().each(function(index, item) {
-    if(item.classList != undefined) {
-      if(item.classList.contains("yt-simple-endpoint") == true) {
-        currentSongAuthors.push(item.innerHTML)
-      }
-    }
-  })
-  
-  var currentSongAuthorString = ""
-
-  if(currentSongAuthors.length == 0 || currentSongAuthors.length == 1) {
-      currentSongAuthorString = $(".byline.ytmusic-player-bar").contents().first().text()
-  } else {
-    for(var i = 0; i < currentSongAuthors.length; i++) {
-      if(i == currentSongAuthors.length -1) {
-        currentSongAuthorString = currentSongAuthors[i] + " - " + currentSongAuthorString
-      } else {
-        if(i == 0) {
-          currentSongAuthorString = currentSongAuthorString + currentSongAuthors[i]
-        } else {
-          currentSongAuthorString = currentSongAuthorString + ", " + currentSongAuthors[i]
+    //* Get all authors
+    $(".byline.ytmusic-player-bar").contents().each(function(index, item) {
+      if(item.classList != undefined) {
+        if(item.classList.contains("yt-simple-endpoint") == true) {
+          songAuthors.push(item.innerHTML)
         }
       }
+    })
+    
+    //* If no authors found in previous method use this
+    if(songAuthors.length == 0 || songAuthors.length == 1) {
+      //* Clear old list
+      songAuthors = []
+      songAuthors.push($(".byline.ytmusic-player-bar").contents().first().text())
     }
-  }
 
-  const data = {
-    connected: true,
-    service: "ytm",
-    url: urlForVideo,
-    currentSongTitle: $(".title.style-scope.ytmusic-player-bar").html(),
-    currentSongAuthor: currentSongAuthorString,
-    currentSongStartTime: getSeconds(calcDifference[0]),
-    currentSongEndTime: getSeconds(calcDifference[1]),
-    currentSongCover: $(".image.style-scope.ytmusic-player-bar").attr("src")
-  };
+    var startTime = Date.now();
+    var endTime =
+      Math.floor(startTime / 1000) -
+      songStartTime +
+      songEndTime;
 
+    const data = {
+      connected: true,
+      service: "ytm",
+      currentSongTitle: songTitle,
+      currentSongAuthors: songAuthors,
+      currentSongStartTimeSeconds: songStartTime,
+      currentSongStartTime: startTime,
+      currentSongEndTime: endTime,
+      currentSongCover: songCover
+    }
+
+    //* Send data to app/server
     $.ajax({
       async: true,
       crossDomain: true,
@@ -64,15 +75,14 @@ function updateData() {
       processData: false,
       data: JSON.stringify(data)
     })
-
   }
 }
 
 //* Used to extract seconds from Syntax 
 //* 1:39 => 99
 function getSeconds(string) {
-  const s = string.split(":");
+  const s = string.split(":")
 
-  const seconds = +s[0] * 60 + +s[1];
-  return seconds;
+  const seconds = +s[0] * 60 + +s[1]
+  return seconds
 }
--- src/presences/handleYTM.js
+++ src/presences/handleYTM.js
@@ -1,6 +1,7 @@
 const constants = require("../util/constants.js");
 const { ytm_client_id } = require("../config.json");
 const Config = require("electron-config");
+const chalk = require("chalk");
 const userData = new Config({
   name: 'userData'
 })
@@ -16,50 +17,133 @@ let startTime = new Date(),
   endTime = new Date(),
   lastStartingTime;
 
-async function updatePresence(data) {
-  if (data.currentSongStartTime == lastStartingTime) {
-    if ((data.currentSongAuthor && data.currentSongTitle) != undefined &&
-    (data.currentSongAuthor && data.currentSongTitle) != "") {
-      constants.menuBar.tray.setTitle("");
-      constants.ytmrpc.setActivity({
-        details: entities.decode(data.currentSongTitle),
-        state: entities.decode(data.currentSongAuthor),
-        smallImageKey: "pause",
-        smallImageText: "Playback paused.",
-        largeImageKey: "ytm_lg",
-        instance: true
-      });
-    }
-  } else {
-    userData.set('data', data)
-    startTime = new Date();
-    endTime =
-      Math.floor(startTime / 1000) -
-      data.currentSongStartTime +
-      data.currentSongEndTime;
-    lastTitle = data.currentSongTitle;
-    lastStartingTime = data.currentSongStartTime;
 
-    if (
-      (data.currentSongAuthor && data.currentSongTitle) != undefined &&
-      data.currentSongAuthor != "" &&
-      data.currentSongTitle != ""
-    ) {
-      if(userSettings.get('titleMenubar')) {
-        constants.menuBar.tray.setTitle(entities.decode(data.currentSongTitle));
-      } else constants.menuBar.tray.setTitle("")
-      constants.ytmrpc.setActivity({
-        details: entities.decode(data.currentSongTitle),
-        state: entities.decode(data.currentSongAuthor),
+let lastSongTitle = null,
+  lastSongAuthors = [],
+  currentSongAuthorString,
+  lastSongStartTime,
+  playbackWasPaused,
+  playbackPausedDebug = 0,
+  nextSongPauseDebug = 0;
+
+async function updatePresence(data) {
+  //console.log(data)
+
+  var currentSongTitle = data.currentSongTitle,
+  currentSongAuthors = data.currentSongAuthors,
+  currentSongStartTime = data.currentSongStartTime,
+  currentSongStartTimeSeconds = data.currentSongStartTimeSeconds,
+  currentSongEndTime = data.currentSongEndTime
+
+  //* Create author string from author array
+  currentSongAuthors.forEach((author, index, authors) => {
+    if(index == 0) {
+      currentSongAuthorString = author
+    } else if(index < authors.length - 2) {
+      currentSongAuthorString = currentSongAuthorString + ", " + author
+    } else if(index < authors.length - 1) {
+      currentSongAuthorString = currentSongAuthorString + " and " + author
+    } else {
+      currentSongAuthorString = currentSongAuthorString + " - " + author
+    }
+  });
+
+  //* Update songTitle if changed
+  if(lastSongTitle != currentSongTitle) {
+    //* Only update if authors changed aswell (Debug for Presence)
+    if(lastSongAuthors.join() != currentSongAuthors.join()) {
+      lastSongTitle = currentSongTitle
+      lastSongAuthors = currentSongAuthors
+      lastSongStartTime = currentSongStartTimeSeconds
+
+      nextSongPauseDebug = 1
+
+      if(playbackWasPaused) playbackWasPaused = false
+      
+      console.log(constants.consolePrefix + chalk.yellow('Song changed, now playing: ') + chalk.green(currentSongTitle) + chalk.yellow('.'))
+
+      //* Set menuBar title if enabled
+      if(userSettings.get('titleMenubar')) constants.menuBar.tray.setTitle(currentSongTitle)
+
+      //* Update ytm presence
+      updateYTMPresence({
+        details: entities.decode(currentSongTitle),
+        state: entities.decode(currentSongAuthorString),
         smallImageKey: "play",
         smallImageText: "Playing back.",
         largeImageKey: "ytm_lg",
-        startTimestamp: startTime,
-        endTimestamp: endTime,
+        startTimestamp: currentSongStartTime,
+        endTimestamp: currentSongEndTime,
         instance: true
-      });
+      })
     }
+  } else {
+    if(nextSongPauseDebug == 0) {
+      if(lastSongStartTime == currentSongStartTimeSeconds) {
+        if(!playbackWasPaused) {
+          if(playbackPausedDebug == 1) {
+            playbackPausedDebug = 0;
+            playbackWasPaused = true
+            lastSongStartTime = currentSongStartTimeSeconds
+      
+            console.log(constants.consolePrefix + chalk.yellow('Song paused.'))
+      
+            //* Update ytm presence
+            updateYTMPresence({
+              details: entities.decode(currentSongTitle),
+              state: entities.decode(currentSongAuthorString),
+              smallImageKey: "pause",
+              smallImageText: "Playing paused.",
+              largeImageKey: "ytm_lg",
+              instance: true
+            })
+          } else {
+            playbackPausedDebug++
+          }
+        }
+      } else if(playbackWasPaused) {
+  
+        console.log(constants.consolePrefix + chalk.yellow('Song resumed.'))
+  
+        //* Update ytm presence
+        updateYTMPresence({
+          details: entities.decode(currentSongTitle),
+          state: entities.decode(currentSongAuthorString),
+          smallImageKey: "play",
+          smallImageText: "Playing back.",
+          largeImageKey: "ytm_lg",
+          startTimestamp: currentSongStartTime,
+          endTimestamp: currentSongEndTime,
+          instance: true
+        })
+  
+        playbackWasPaused = false
+      } else {
+        var difference = Math.abs(lastSongStartTime - currentSongStartTimeSeconds +1);
+        if(difference != 0) {
+          console.log(constants.consolePrefix + chalk.yellow(`Song time changed. Difference: ${difference} seconds.`))
+          //* Update ytm presence
+          updateYTMPresence({
+            details: entities.decode(currentSongTitle),
+            state: entities.decode(currentSongAuthorString),
+            smallImageKey: "play",
+            smallImageText: "Playing back.",
+            largeImageKey: "ytm_lg",
+            startTimestamp: currentSongStartTime,
+            endTimestamp: currentSongEndTime,
+            instance: true
+          })
+        }
+      }
+    } else {
+      nextSongPauseDebug--
+    }
+    lastSongStartTime = currentSongStartTimeSeconds
   }
+}
+
+function updateYTMPresence(data) {
+  constants.ytmrpc.setActivity(data);
 }
 
 constants.ytmrpc.on("ready", () => {
@@ -69,3 +153,6 @@ constants.ytmrpc.on("ready", () => {
 exports.updatePresence = updatePresence;
 
 constants.ytmrpc.login(ytm_client_id).catch(console.error);
+
+
+//* Catch Discord RPC errors
