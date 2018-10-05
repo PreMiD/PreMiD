c58e7d2ccf91758f81fc0e2109a76133e3bf02ad
Timeraa <29104008+Timeraa@users.noreply.github.com>
2018-10-05 18:35:19 +0200

    fixes

--- createInstaller.js
+++ createInstaller.js
@@ -4,6 +4,7 @@ resultPromise = electronInstaller.createWindowsInstaller({
   appDirectory: '/out/YT Presence-win32-64x',
   outputDirectory: '/dist/installerWindows',
   authors: 'Timeraa',
+  exe: './YT-Presence.exe'
 });
 
 resultPromise.then(() => console.log("It worked!"), (e) => console.log(`No dice: ${e.message}`));
