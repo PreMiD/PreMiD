8e3b7b0418327fbece33d6e3ae4795c376ad4c93
Finn <mail4finn@gmail.com>
2018-11-25 14:39:32 +0100

    Added a secret :3

--- Extension/js/background.js
+++ Extension/js/background.js
@@ -19,7 +19,7 @@ setInterval(() => {
     //* Create config if not already
     if(!result.options) chrome.storage.sync.set({options: {enabled: true, youtube: true, youtubeMusic: true, twitch: true, soundcloud: true, netflix: true, kissanime: true, jkanime: true, titleMenubar: true, mediaControls: true, checkForUpdates: true, systemStartup: true}})
     
-    allowedURL = ["www.youtube.com", "music.youtube.com", "twitch.tv", "soundcloud.com", "netflix.com", "kissanime.ac", "kissanime.ru", "jkanime.net", "fimfiction.net"]
+    allowedURL = ["www.youtube.com", "music.youtube.com", "twitch.tv", "soundcloud.com", "netflix.com", "kissanime.ac", "kissanime.ru", "jkanime.net", "fimfiction.net", "hentaihaven.org"]
     var options = result.options
     if(!options.enabled) allowedURL = []
     if(!options.youtube) allowedURL.remove("www.youtube.com")
