874dd9ff62f5ff808a12cb1803222dc484dee6e0
Finn <mail4finn@gmail.com>
2018-11-26 23:01:00 +0100

    SuperAnimes support

--- Extension/js/util/socketConnector.js
+++ Extension/js/util/socketConnector.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
-var allowedURL = ["www.youtube.com", "music.youtube.com", "twitch.tv", "soundcloud.com", "www.netflix.com", "kissanime.ac", "kissanime.ru", "jkanime.net", "fimfiction.net", "www.crunchyroll.com"]
-=======
 
-var allowedURL = ["www.youtube.com", "music.youtube.com", "twitch.tv", "soundcloud.com", "netflix.com", "kissanime.ac", "kissanime.ru", "jkanime.net", "fimfiction.net", "www.crunchyroll.com", "www.rabb.it", "www.masterani.me", "www.youtube.com/tv"]
->>>>>>> cfe252269954b6394004aa546b7744fdd8137a2b
+var allowedURL = ["www.youtube.com", "music.youtube.com", "twitch.tv", "soundcloud.com", "netflix.com", "kissanime.ac", "kissanime.ru", "jkanime.net", "fimfiction.net", "www.crunchyroll.com", "www.rabb.it", "www.masterani.me", "www.youtube.com/tv", "www.superanimes.site"]
 
 if(allowedURL.includes(document.location.host)) {  
   //* Create socket connection to application
@@ -73,6 +69,9 @@ function getService() {
       break
     case "www.masterani.me":
       return "MasterAnime"
+      break
+    case "www.superanimes.site":
+      return "SuperAnimes"
       break
     default:
       return null
