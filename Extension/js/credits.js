d0f5b803fa903264feb843534dbdff97f3435d0f
Timeraa <29104008+Timeraa@users.noreply.github.com>
2018-11-27 21:34:07 +0100

    Credits (WIP)

---
+++ Extension/js/credits.js
@@ -0,0 +1,15 @@
+$(document).ready(async function() {
+  fetch('https://api.premid.app/credits').then(async response => {
+    var result = await response.json()
+    var firstItemInRow;
+    var currItemInRow = 0
+    result.forEach(element => {
+      if(currItemInRow == 0) firstItemInRow = element
+      if(currItemInRow == 1) {
+        $('<tr><td><div class="credit-panel card-panel"><img draggable="false" src="' + element.avatarURL + '"><h1 class="fitty">' + element.name + '</h1><h2>' + element.type + '</h2></div></td><td><div class="credit-panel card-panel"><img draggable="false" src="' + firstItemInRow.avatarURL + '"><h1 class="fitty">' + firstItemInRow.name + '</h1><h2>' + firstItemInRow.type + '</h2></div></td></tr>').appendTo('#credits table')
+        currItemInRow = 0
+      } else 
+      currItemInRow++;
+    });
+  })
+})
