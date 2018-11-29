2edfab25d79af0086798ad8e921029ab4c6e1f88
Timeraa <29104008+Timeraa@users.noreply.github.com>
2018-11-29 21:35:42 +0100

    New Update tab

--- Extension/js/updated.js
+++ Extension/js/updated.js
@@ -1,27 +1,29 @@
-document.getElementById('version').innerHTML = 'V' + chrome.runtime.getManifest().version
-document.getElementById('updatedYTPUpdated').innerHTML = chrome.i18n.getMessage("updatedYTPUpdated")
-document.getElementById('updatedWhatsNew').innerHTML = chrome.i18n.getMessage("updatedWhatsNew")
+$(document).ready(async function() {
+  $('.Pheading').html(await getString("tab.updated.heading"))
+  $('.PwhatsNew').html(await getString("tab.updated.subHeading.whatsNew"))
+  $('.PwhatChanged').html(await getString("tab.updated.subHeading.whatChanged"))
+  $('.PgotIt').html(await getString("tab.updated.gotIt"))
+  $('.PlikeThisProject').html((await getString("tab.installed.likeThisProject")).replace("$1", '<a target="_blank" href="https://github.com/Timeraa/PreMiD">GitHub</a>'))
+  
+  var hasNextChanged = true,
+  index = 0
+  while(hasNextChanged) {
+    if(await getString("tab.updated.added" + index) != null) {
+      var item = document.getElementById('WhatsNewList').appendChild(document.createElement('li'))
+      item.innerHTML = await getString("tab.updated.added" + index)
+    } else hasNextChanged = false;
+    index++
+  }
+  
+  hasNextChanged = true
+  index = 0
+  while(hasNextChanged) {
+    if(await getString("tab.updated.changed" + index) != null) {
+      var item = document.getElementById('WhatChangedList').appendChild(document.createElement('li'))
+      item.innerHTML = await getString("tab.updated.changed" + index)
+    } else hasNextChanged = false;
+    index++
+  }
+})
 
-var hasNextChanged = true,
-index = 0
-while(hasNextChanged) {
-  if(chrome.i18n.getMessage("updatedAdded" + index) != "") {
-    var item = document.getElementById('updatedWhatsNewList').appendChild(document.createElement('li'))
-    item.innerHTML = chrome.i18n.getMessage("updatedAdded" + index)
-  } else hasNextChanged = false;
-  index++
-}
-
-document.getElementById('updatedWhatChanged').innerHTML = chrome.i18n.getMessage("updatedWhatChanged")
-
-var hasNextChanged = true,
-index = 0
-while(hasNextChanged) {
-  if(chrome.i18n.getMessage("updatedChanged" + index) != "") {
-    var item = document.getElementById('updatedWhatChangedList').appendChild(document.createElement('li'))
-    item.innerHTML = chrome.i18n.getMessage("updatedChanged" + index)
-  } else hasNextChanged = false;
-  index++
-}
-
-document.getElementById('installedSupportMe').innerHTML = chrome.i18n.getMessage("installedSupportMe")
+$('.gotIt').click(function() { window.close()})
