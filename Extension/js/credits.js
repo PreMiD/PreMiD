8eb0d43287935d1a0ba010690b9231dde441a6c3
Voknehzyr <gamesforevaigor@gmail.com>
2018-11-30 20:19:13 +0300

    Popup revision
    New styles and improvements for current elements.

--- Extension/js/credits.js
+++ Extension/js/credits.js
@@ -6,7 +6,7 @@ $(document).ready(async function() {
     result.forEach(element => {
       if(currItemInRow == 0) firstItemInRow = element
       if(currItemInRow == 1) {
-        $('<tr><td><div class="credit-panel card-panel"><img draggable="false" src="' + element.avatarURL + '"><h1 class="fitty">' + element.name + '</h1><h2>' + element.type + '</h2></div></td><td><div class="credit-panel card-panel"><img draggable="false" src="' + firstItemInRow.avatarURL + '"><h1 class="fitty">' + firstItemInRow.name + '</h1><h2>' + firstItemInRow.type + '</h2></div></td></tr>').appendTo('#credits table')
+        $('<div class="col s6"><div class="credit-panel card-panel"><img draggable="false" src="' + element.avatarURL + '"><h1 class="fitty">' + element.name + '</h1><h2>' + element.type + '</h2></div></td><td><div class="credit-panel card-panel"><img draggable="false" src="' + firstItemInRow.avatarURL + '"><h1 class="fitty">' + firstItemInRow.name + '</h1><h2>' + firstItemInRow.type + '</h2></div></div>').appendTo('#credits #credits_container')
         currItemInRow = 0
       } else 
       currItemInRow++;
