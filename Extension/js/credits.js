b0d4eb7c3fadb820bec7172ab25acb1e1c778b2a
Timeraa <29104008+Timeraa@users.noreply.github.com>
2018-12-02 01:51:53 +0100

    credits fix for uneven count

--- Extension/js/credits.js
+++ Extension/js/credits.js
@@ -4,19 +4,8 @@ $(document).ready(async function () {
     result = result.sort((a, b) => {
       return b.rolePosition - a.rolePosition
     })
-    var firstItemInRow;
-    var currItemInRow = 0
     result.forEach(element => {
-      if (currItemInRow == 0) firstItemInRow = element
-      if (currItemInRow == 1) {
-        var nicknameColor;
-        if(element.patronColor) {
-          var nicknameColor = ""
-        }
-        $(`<div class="usercard" style="--hoverColor: ` + element.roleColor + `"><div><div class="credits_avatar"><img draggable="false" src="` + element.avatar + `"></div><div class="credits_text">` + getNameColor(element) + `<p>` + element.role + `</p><p> ${element.patronColor != "#fff" && element.role != "Patron" ? "Patron" : ""}</p></div></div></div><div class="usercard" style="--hoverColor: ` + firstItemInRow.roleColor + `"><div><div class="credits_avatar"><img draggable="false" src="` + firstItemInRow.avatar + `"></div><div class="credits_text">` + getNameColor(firstItemInRow) + `<p>` + firstItemInRow.role + `</p><p> ${firstItemInRow.patronColor != "#fff" && firstItemInRow.role != "Patron" ? "Patron" : ""}</p></div></div></div>`).appendTo('#credits .credits_container')
-        currItemInRow = 0
-      } else
-        currItemInRow++;
+        $(`<div class="usercard" style="--hoverColor: ` + element.roleColor + `"><div><div class="credits_avatar"><img draggable="false" src="` + element.avatar + `"></div><div class="credits_text">` + getNameColor(element) + `<p>` + element.role + `</p><p> ${element.patronColor != "#fff" && element.role != "Patron" ? "Patron" : ""}</p></div></div></div>`).appendTo('#credits .credits_container')
     });
   }).catch(err => {
     $(`<div class="usercard noHover"><div class="credits_text"><h6>Error</h6><p>Could not load credits.</p></div></div>`).appendTo('#credits .credits_container')
