38fe1c60f641732ff2a8f656c06ebec197f5aff7
Timeraa <29104008+Timeraa@users.noreply.github.com>
2018-12-07 17:53:07 +0100

    Firefox stuffs again

--- Firefox_Extension/js/background.js
+++ Firefox_Extension/js/background.js
@@ -14,9 +14,12 @@ browser.runtime.onInstalled.addListener(function(details) {
 //* Tab Priority™ variables
 var lastTabId = null,
 lastTabPriorityLock = 0,
+priorityTabId = null,
 priorityTab = null,
 allowedTabs = ["www.youtube.com"],
 tabPriorityInterval = null
+
+var oldOptions
 
 var socket = io.connect("http://localhost:3020/")
 socket.on("connect", function() {
@@ -30,7 +33,7 @@ socket.on("disconnect", function() {
 
 socket.on("mediaKeyHandler", function(data) {
   //* Media control buttons
-  if(priorityTab) browser.tabs.sendMessage(priorityTab, {mediaKeys: data.playback})
+  if(priorityTabId) browser.tabs.sendMessage(priorityTabId, {mediaKeys: data.playback})
 })
 
 //* Forward the presence data received from Presence script to application
@@ -45,28 +48,98 @@ browser.runtime.onMessage.addListener(function(data, sender) {
  * Handles tab changes.
  */
 async function tabPriority() {
-  browser.tabs.query({active: true})
-  .then(function(tabs) {
-    allowedTabs.forEach(function(url) {
-      if(tabs[0].url.indexOf(url) > -1) {
-        if(tabs[0].id == lastTabId) {
-          if(lastTabPriorityLock == 5) {
-            if(priorityTab != tabs[0].id) {
-              if(priorityTab != null) 
-                browser.tabs.sendMessage(priorityTab, {tabPriority: false})
-              priorityTab = tabs[0].id
-            }
-          } 
+  browser.storage.sync.get(['options'], function(result) {
+    allowedTabs = ["www.youtube.com"]
 
-          lastTabPriorityLock++
-        } else {
-          lastTabId = tabs[0].id
-          lastTabPriorityLock = 0
-        }
-      } else lastTabPriorityLock = 0
+    var options = result.options
+    if(!options.enabled) {
+      allowedTabs = []
+      priorityTab = null
+      priorityTabId = null
+      lastTabId = null
+    }
+
+    updateTabPriorityService("youtube", "www.youtube.com", options)
+
+    browser.tabs.query({active: true})
+    .then(function(tabs) {
+      allowedTabs.forEach(function(url) {
+        if(tabs[0].url.indexOf(url) > -1) {
+          if(tabs[0].id == lastTabId) {
+            if(lastTabPriorityLock == 5) {
+              if(priorityTabId != tabs[0].id) {
+                if(priorityTabId != null) 
+                  browser.tabs.sendMessage(priorityTabId.id, {tabPriority: false})
+                  priorityTabId = tabs[0].id
+                  priorityTab = tabs[0]
+              }
+            } 
+  
+            lastTabPriorityLock++
+          } else {
+            lastTabId = tabs[0].id
+            lastTabPriorityLock = 0
+          }
+        } else lastTabPriorityLock = 0
+      })
+  
+      if(priorityTabId)
+        browser.tabs.sendMessage(priorityTabId, {tabPriority: true})
     })
 
-    if(priorityTab)
-      browser.tabs.sendMessage(priorityTab, {tabPriority: true})
+    //* Update settings in application if changed
+    if(oldOptions == null || !isEquivalent(result.options, oldOptions)) {
+      oldOptions = result.options
+      socket.emit('settingsChange', result)
+    }
   })
+}
+
+/**
+ * Remove service if disabled
+ * @param {String} service Service name in options.json
+ * @param {Strng} serviceURL Service URL to handle
+ * @param {Object} options Options objects
+ */
+function updateTabPriorityService(service, serviceURL, options) {
+  if(!options[service]) {
+    var index = allowedTabs.indexOf(serviceURL)
+    if(index != -1) allowedTabs.splice(index, 1)
+    if(priorityTab != null && priorityTab.url.includes(serviceURL)) {
+      priorityTabId = null
+      priorityTab = null
+      lastTabId = null
+    } 
+  }
+}
+
+/**
+ * Test if object is equal to object
+ * @param {Object} a 
+ * @param {Object} b 
+ */
+function isEquivalent(a, b) {
+  // Create arrays of property names
+  var aProps = Object.getOwnPropertyNames(a);
+  var bProps = Object.getOwnPropertyNames(b);
+
+  // If number of properties is different,
+  // objects are not equivalent
+  if (aProps.length != bProps.length) {
+      return false;
+  }
+
+  for (var i = 0; i < aProps.length; i++) {
+      var propName = aProps[i];
+
+      // If values of same property are not equal,
+      // objects are not equivalent
+      if (a[propName] !== b[propName]) {
+          return false;
+      }
+  }
+
+  // If we made it this far, objects
+  // are considered equivalent
+  return true;
 }
