1f2054beb70d3b67db4be18c261974ad7dc50683
Timeraa <29104008+Timeraa@users.noreply.github.com>
2019-01-02 21:25:09 +0100

    Organization stuff

--- website/test.js
+++
@@ -1,5 +0,0 @@
-var request = require('request')
-
-request.post('https://discordapp.com/api/v6/users/@me', {form: '13osaWpODiNl5xq7w0NQ0A9dap7O2a'}, (error, response, body) => {
-  console.log(body)
-})
