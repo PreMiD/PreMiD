1c68b0234303a724113dca1ff7b047862f4d8022
Timeraa <29104008+Timeraa@users.noreply.github.com>
2019-01-02 21:26:07 +0100

    Organization stuff 2

--- DiscordBot/util/eventLoader.js
+++
@@ -1,7 +0,0 @@
-
-const reqEvent = (event) => require(`../events/${event}`)
-
-module.exports = (client) => {
-  client.on('ready', () => reqEvent('ready')(client));
-  client.on('message', reqEvent('message'));
-};
