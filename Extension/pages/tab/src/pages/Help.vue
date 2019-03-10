1f8d3aa7f492c83f3e664a58e2faa8a33a50b0e9
MulverineX <mulverin3@gmail.com>
2019-03-09 17:48:45 -0700

    Tab Initilization

---
+++ Extension/pages/tab/src/pages/Help.vue
@@ -0,0 +1,13 @@
+<template>
+    <p>Help</p>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ Extension/pages/tab/src/pages/Share.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="main">
+        <p>Share</p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "share",
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+
+<style lang="less" scoped>
+@import "./../stylesheets/colors.less";
+.main {
+  height: 75%;
+  overflow: scroll;
+}
+</style>
