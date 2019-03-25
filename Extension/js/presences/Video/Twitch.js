99d14139d2ed88063c87c5b9495a816ade4782d9
Fruxh <mail4finn@gmail.com>
2019-03-25 20:48:52 +0100

    hotfix

--- Extension/js/presences/Video/Twitch.js
+++ Extension/js/presences/Video/Twitch.js
@@ -1,25 +1,25 @@
 var playback = false,
-videoTitle,
-videoAuthor,
-videoTimestamps,
-playbackBoolean,
-smallImageKey,
-smallImageText,
-urlForVideo,
-lastURL = null
+	videoTitle,
+	videoAuthor,
+	videoTimestamps,
+	playbackBoolean,
+	smallImageKey,
+	smallImageText,
+	urlForVideo,
+	lastURL = null;
 
 /**
  * Handles Media Key controls
  * @param {data} data Data passed by socketConnector.js
  */
 async function handleMediaKeys(data) {
-  if(playback) {
-    switch (data.mediaKeys) {
-      case "pause":
-        playbackBoolean ? $(".player-video video")[0].pause() : $(".player-video video")[0].play()
-        break
-    }
-  }
+	if (playback) {
+		switch (data.mediaKeys) {
+			case 'pause':
+				playbackBoolean ? $('.player-video video')[0].pause() : $('.player-video video')[0].play();
+				break;
+		}
+	}
 }
 
 /**
@@ -27,44 +27,46 @@ async function handleMediaKeys(data) {
  * to the background.js for further interaction
  */
 async function updateData() {
-  urlForVideo = document.location.href;
-  
-  playback = $(".player-video video")[0] != undefined && $(".video-info__container .tw-mg-b-05 p").innerHTML.split(" · ")[0].length > 0
+	urlForVideo = document.location.href;
 
-  //* If page has all required propertys
-  if(playback) {
-    if (urlForVideo != lastURL) {
-      lastURL = urlForVideo;
-      startTimeStamp = Math.floor(Date.now() / 1000);
-    }
+	playback = $('.player-video video')[0] != undefined && $('.tw-font-size-4').innerText;
 
-    videoTitle = $(".video-info__container .tw-mg-b-05 p").innerHTML.split(" · ")[0]
-    videoAuthor = $(".channel-header__user-avatar h5").innerHTML;
-    playbackBoolean = !$(".player-video video").paused;
-    smallImageKey = playbackBoolean ? 'play' : 'pause'
-    smallImageText = playbackBoolean ? await getString("presence.playback.playing") : await getString("presence.playback.paused")
+	//* If page has all required propertys
+	if (playback) {
+		if (urlForVideo != lastURL) {
+			lastURL = urlForVideo;
+			startTimeStamp = Math.floor(Date.now() / 1000);
+		}
 
-    var data = {
-      clientID: '501021996336021504',
-      presenceData: {
-        details: $('<div/>').html(videoTitle).text(),
-        state: $('<div/>').html(videoAuthor).text(),
-        largeImageKey: 'twitch_lg',
-        largeImageText: chrome.runtime.getManifest().name + ' V' + chrome.runtime.getManifest().version,
-        smallImageKey: smallImageKey,
-        smallImageText: smallImageText,
-      },
-      trayTitle: $('<div/>').html(videoTitle).text(),
-      playback: playbackBoolean,
-      service: 'Twitch'
-    }
+		videoTitle = $('.tw-font-size-4').innerText;
+		videoAuthor = $('.channel-header__user-avatar h5').innerHTML;
+		playbackBoolean = !$('.player-video video').paused;
+		smallImageKey = playbackBoolean ? 'play' : 'pause';
+		smallImageText = playbackBoolean
+			? await getString('presence.playback.playing')
+			: await getString('presence.playback.paused');
 
-    if(playbackBoolean) {
-      data.presenceData.startTimestamp = startTimeStamp
-    } else {
-      delete data.presenceData.startTimestamp
-    }
+		var data = {
+			clientID: '501021996336021504',
+			presenceData: {
+				details: $('<div/>').html(videoTitle).text(),
+				state: $('<div/>').html(videoAuthor).text(),
+				largeImageKey: 'twitch_lg',
+				largeImageText: chrome.runtime.getManifest().name + ' V' + chrome.runtime.getManifest().version,
+				smallImageKey: smallImageKey,
+				smallImageText: smallImageText
+			},
+			trayTitle: $('<div/>').html(videoTitle).text(),
+			playback: playbackBoolean,
+			service: 'Twitch'
+		};
 
-    chrome.runtime.sendMessage({presence: data})
-  }
+		if (playbackBoolean) {
+			data.presenceData.startTimestamp = startTimeStamp;
+		} else {
+			delete data.presenceData.startTimestamp;
+		}
+
+		chrome.runtime.sendMessage({ presence: data });
+	}
 }
