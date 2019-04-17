f28732904d71e08d7f8d56f4d46e999d4bb039c6
Timeraa <29104008+Timeraa@users.noreply.github.com>
2019-04-17 21:06:28 +0200

    ⚙️ Small rework of settingsHandler

--- src/util/settingsHandler.js
+++ src/util/settingsHandler.js
@@ -3,23 +3,23 @@ const options = new Config({
 	name: 'options'
 });
 
-var mediaControls = require('./shortcutHandler');
+var mediaControls = require('./shortcutHandler'),
+	{ success } = require('../util/debug');
 
 module.exports = async (data) => {
-	console.log(data);
+	success('Saved settings from extension');
 	if (options.get('titleMenubar') != data.titleMenubar) {
 		options.set('titleMenubar', data.titleMenubar);
 		if (PLATFORM == 'darwin' && !data.titleMenubar) TRAY.setTitle('');
 	}
 
-	if (options.get('autoUpdate') != data.checkForUpdates) options.set('autoUpdate', data.checkForUpdates);
+	if (options.get('autoUpdate') != data.autoUpdate) options.set('autoUpdate', data.autoUpdate);
 
-	if (options.get('autoLaunch') != data.systemStartup) {
-		options.set('autoLaunch', data.systemStartup);
+	if (options.get('autoLaunch') != data.autoLaunch) {
+		options.set('autoLaunch', data.autoLaunch);
 		require('./autoLaunch').init();
 	}
 
-	console.log(options.get('mediaKeys') != data.mediaKeys);
 	if (options.get('mediaKeys') != data.mediaKeys) {
 		options.set('mediaKeys', data.mediaKeys);
 		if (data.mediaKeys) mediaControls.register();
@@ -30,6 +30,4 @@ module.exports = async (data) => {
 		options.set('titleMenubar', data.titleMenubar);
 		if (!data.titleMenubar) TRAY.setTitle('');
 	}
-
-	//console.log(data.options)
 };
