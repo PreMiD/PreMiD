ed9c5a0dc73434c5a5824124df07be26a3370cd9
Timeraa <metz.florian@arcor.de>
2019-08-01 18:02:17 +0200

    ♻️ Refactored TypeScript compiler & devMode

--- devMode.ts
+++ devMode.ts
@@ -1,44 +1,99 @@
-console.clear();
-
-import { copySync, removeSync, writeFileSync } from "fs-extra";
-import { extname, basename } from "path";
+import * as ts from "typescript";
+import chalk from "chalk";
+import { ChildProcess, spawn } from "child_process";
+import { removeSync, writeFileSync, copySync } from "fs-extra";
+import { extname } from "path";
 import * as glob from "fast-glob";
 
-(async () => {
-  await removeSync("dist/app/node_modules");
+//* Current child process = null
+var currChild: ChildProcess = null;
 
+//* Format Host
+const formatHost: ts.FormatDiagnosticsHost = {
+  getCanonicalFileName: path => path,
+  getCurrentDirectory: ts.sys.getCurrentDirectory,
+  getNewLine: () => ts.sys.newLine
+};
+
+//* Create ts program
+const createProgram = ts.createSemanticDiagnosticsBuilderProgram;
+
+//* Create Watch compoile host
+const host = ts.createWatchCompilerHost(
+  "./tsconfig.json",
+  {},
+  ts.sys,
+  createProgram,
+  null,
+  fileChange
+);
+
+if (process.argv.length == 3 && process.argv[2] == "compile")
+  prepareDist().then(() => process.exit());
+//* Create watch program
+else ts.createWatchProgram(host);
+
+async function fileChange(diagnostic: ts.Diagnostic) {
+  //* TS compilation != done
+  if (diagnostic.code !== 6194) {
+    //* If error
+    if (diagnostic.code === 6193)
+      console.log(chalk.redBright(ts.formatDiagnostic(diagnostic, formatHost)));
+    else
+      console.log(
+        chalk.blueBright(ts.formatDiagnostic(diagnostic, formatHost))
+      );
+    return;
+  }
+
+  //* Kill old child if exists && alive
+  if (currChild && !currChild.killed) currChild.kill();
+
+  //* Run devMode script
+  prepareDist();
+
+  //* Clear console
+  console.clear();
+
+  //* Run child
+  currChild = spawn("npm run --silent start", {
+    shell: true,
+    stdio: "inherit"
+  });
+}
+
+//* Prepare dist folder
+async function prepareDist() {
+  //* Select files
   var dist = await glob("dist/app/**/*", { onlyFiles: true }),
     src = await glob("src/**/*", { onlyFiles: true });
 
-  //* Delete js files in dist that are no longer existant in src folder
-  await Promise.all(
+  //* Filter file differences
+  var nDist = dist.map(f => [f.replace("dist/app/", ""), f]);
+  src = src
+    .map(f => f.replace("src/", "").split(".")[0])
+    .filter(sf => nDist.find(d => d[0].split(".")[0] == sf));
+
+  //* Old files, delete
+  Promise.all(
     dist
-      .map(f => [
-        f.replace("dist/app/", "").replace(basename(f), basename(f, ".js")),
-        f
-      ])
-      .filter(
-        d =>
-          !src
-            .map(f =>
-              f.replace("src/", "").replace(basename(f), basename(f, ".ts"))
-            )
-            .includes(d[0])
-      )
-      .filter(d => extname(d[1]) !== ".map")
-      .map(f => removeSync(f[1]))
+      .filter(f => !src.includes(f.replace("dist/app/", "").split(".")[0]))
+      .map(f => removeSync(f))
   );
 
+  //* Copy package.json (minified)
   var packageJSON = require("./package.json"),
-    srcPackageJSON;
+    srcPackageJSON = packageJSON;
 
-  srcPackageJSON = packageJSON;
-
+  //* Delete scripts, not needed
   delete srcPackageJSON.scripts;
+
+  //* only electron as devDependency
   srcPackageJSON.devDependencies = {
     electron: packageJSON.devDependencies.electron
   };
 
+  //* Write file
   await writeFileSync(
     "./src/package.json",
     JSON.stringify(srcPackageJSON, null, 2)
@@ -50,4 +105,4 @@ import * as glob from "fast-glob";
       return extname(path) !== ".ts";
     }
   });
-})();
+}
