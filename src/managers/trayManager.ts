d93b76a664ba2a608c211bbe6c6ffcdef504f722
Florian Metz <timeraa@Florians-Mac-mini.local>
2019-08-06 03:32:53 +0200

    🐛 Make Tray icon reflect OS appearance

--- src/managers/trayManager.ts
+++ src/managers/trayManager.ts
@@ -1,5 +1,6 @@
 import { Menu, Tray } from "electron";
 import { join } from "path";
+import { platform } from "os";
 
 export var tray: Tray;
 export var trayContextMenu = Menu.buildFromTemplate([
@@ -14,7 +15,9 @@ export var trayContextMenu = Menu.buildFromTemplate([
 export function init() {
   return new Promise<Tray>(function(resolve) {
     //* Create tray
-    tray = new Tray(join(__dirname, "../assets/tray/Icon@2x.png"));
+    if (platform() === "darwin")
+      tray = new Tray(join(__dirname, "../assets/tray/IconTemplate.png"));
+    else tray = new Tray(join(__dirname, "../assets/tray/Icon.png"));
 
     //* Set its context menu
     tray.setContextMenu(trayContextMenu);
