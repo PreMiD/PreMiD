e1c8afd5bafb0b5cf7f9e7b411d6df8d3d3330ac
Florian Metz <metz.florian@arcor.de>
2019-10-04 02:29:01 +0200

    🐛 Icon fix for MacOS

--- pkg.ts
+++ pkg.ts
@@ -87,7 +87,7 @@ import * as ora from "ora";
 
   let icon: string;
 
-  if (response.os == "darwin") icon = "./installer_assets/appIcon.icns";
+  if (response.os == "darwin" || response.os === "current" && platform() === "darwin") icon = "./installer_assets/appIcon.icns";
   if (["ia32", "x64"].includes(response.arch) || platform() === "win32")
     icon = "./installer_assets/appIcon.ico";
 
