44ae3344928dad0116a7c892e2e472ce4ec1cbdc
Florian Metz <metz.florian@arcor.de>
2019-10-05 22:51:20 +0200

    ♻️ More testing

---
+++ .github/deploy.ts
@@ -0,0 +1,45 @@
+import * as Client from "ssh2-sftp-client";
+import * as archiver from "archiver";
+import { platform, arch } from "os";
+import { createWriteStream } from "fs";
+
+let sftp = new Client();
+
+sftp
+  .connect({
+    host: process.env.SSH_HOST,
+    username: process.env.SSH_USERNAME,
+    password: process.env.SSH_PASSWORD
+  })
+  .then(async () => {
+    console.log("Zipping...");
+    let output = createWriteStream("app.zip"),
+      archive = archiver("zip");
+
+    archive.directory(
+      `../dist/PreMiD-${platform()}-${arch()}`,
+      `PreMiD-${platform()}-${arch()}`
+    );
+
+    output.on("close", function() {
+      console.log("Uploading...");
+      sftp
+        .fastPut(
+          "app.zip",
+          `/home/PreMiD/builds/PreMiD-${platform()}-${arch()}.zip`
+        )
+        .then(() => {
+          console.log("Done!");
+          sftp.end();
+        });
+    });
+
+    archive.on("error", function(err) {
+      sftp.end();
+      throw err;
+    });
+
+    archive.pipe(output);
+
+    archive.finalize();
+  });
