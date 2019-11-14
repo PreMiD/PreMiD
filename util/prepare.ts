06d5a08dd036a6dcb6eb60c7f2149a2afa06dd91
Florian Metz <metz.florian@arcor.de>
2019-11-15 00:04:34 +0100

    Testing

---
+++ util/prepare.ts
@@ -0,0 +1,11 @@
+import { readFileSync, writeFileSync } from "fs";
+
+let file = readFileSync("installer_assets/installer.xml", "utf-8");
+
+file = file.replace("VERSION", require("../package.json").version);
+file = file.replace(/(PACKAGEDNAME)/g, process.argv[2]);
+
+if (process.argv[2].includes("ia32"))
+  file = file.replace(/(installer_64bit)/g, "installer_32bit");
+
+writeFileSync("installer.xml", file);
