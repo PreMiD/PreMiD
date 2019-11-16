65cf34b3e02b3b2fafc1adce7b7b8f338ee6ae8d
Florian Metz <metz.florian@arcor.de>
2019-11-16 01:42:29 +0100

    ugh

--- util/prepare.ts
+++ util/prepare.ts
@@ -1,4 +1,6 @@
 import { readFileSync, writeFileSync } from "fs";
+import { parse, stringify } from "ini";
+import { ensureDirSync } from "fs-extra";
 
 let file = readFileSync("installer_assets/installer.xml", "utf-8");
 
@@ -11,3 +13,11 @@ if (process.argv[2].includes("ia32"))
 file = file.replace(/(\.\.\/)/g, "");
 
 writeFileSync("installer.xml", file);
+
+let versionId = "0" + require("./package.json").version.replace(/[.]/g, ""),
+  updateIni = parse(readFileSync("./installer_assets/update.ini", "utf-8"));
+
+updateIni.Update.version_id = versionId;
+
+ensureDirSync("./tmp");
+writeFileSync("./tmp/update.ini", stringify(updateIni));
