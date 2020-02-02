7e20dba63676897335485bcc55ca61d38d999eef
Florian Metz <metz.florian@arcor.de>
2020-02-02 01:44:46 +0100

    F

--- util/prepare.ts
+++ util/prepare.ts
@@ -1,17 +1,7 @@
 import { readFileSync, writeFileSync } from "fs";
-import { parse, stringify } from "ini";
 
 let file = readFileSync("installer_assets/PreMiD-Upgrade.xml", "utf-8");
 
 file = file.replace("VERSION", require("../package.json").version);
 
 writeFileSync("installer_assets/PreMiD-Upgrade.xml", file);
-
-let versionId = (require("../package.json").version as string)
-		.replace(/[.]/g, "")
-		.padStart(4, "0"),
-	updateIni = parse(readFileSync("installer_assets/update.ini", "utf-8"));
-
-updateIni.Update.version_id = versionId;
-
-writeFileSync("installer_assets/update.ini", stringify(updateIni));
