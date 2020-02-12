a54ac832cabadee820efd8a9a4b4613655d799c3
Mateus <41977530+MateusOFCZ@users.noreply.github.com>
2020-02-12 09:59:36 -0300

    presence.js

--- dist/presence.js
+++ dist/presence.js
@@ -8,12 +8,7 @@ var browsingStamp = Math.floor(Date.now()/1000);
 presence.on("UpdateData", () => {
     let presenceData = {
         largeImageKey: "logo",
-        largeImageText = "OGLLE.COM.BR",
-        partyId = "https://oglle.com.br/#Inicio",
-        partySize = 1,
-        partyMax = 1000,
-        spectateSecret = "MTIzNDV8MTIzNDV8MTMyNDU0",
-        joinSecret = "MTI4NzM0OjFpMmhuZToxMjMxMjM= "
+        largeImageText = "OGLLE.COM.BR"
     };
     presenceData.startTimestamp = browsingStamp;
 
@@ -21,23 +16,18 @@ presence.on("UpdateData", () => {
         presenceData.state = "Inicio"
     }
     if(window.location.pathname.toLowerCase() === ("/nossos-programas")) {
-        //presenceData.details = "OGLLE.COM.BR"
         presenceData.state = "Programas"
     }
     if(window.location.pathname.toLowerCase() === ("/servicos")) {
-        //presenceData.details = "OGLLE.COM.BR"
         presenceData.state = "Serviços"
     }
     if(window.location.pathname.toLowerCase() === ("/noticias")) {
-        //presenceData.details = "OGLLE.COM.BR"
         presenceData.state = "Notícias"
     }
     if(window.location.pathname.toLowerCase() === ("/#Contato")) {
-        //presenceData.details = "OGLLE.COM.BR"
         presenceData.state = "Contato"
     }
     if(window.location.pathname.toLowerCase() === ("/anuncios")) {
-        //presenceData.details = "OGLLE.COM.BR"
         presenceData.state = "Ganhando dinheiro com a Oglle.com.br!"
     }
 
