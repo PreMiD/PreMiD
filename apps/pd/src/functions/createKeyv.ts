ce52ce9a97e26168cbd62a9cc4ec6784bc5182e9
Florian Metz <me@timeraa.dev>
2024-02-27 16:25:11 +0100

    fix: redis url passing

--- apps/pd/src/functions/createKeyv.ts
+++ apps/pd/src/functions/createKeyv.ts
@@ -1,7 +1,7 @@
 import Keyv from "keyv";
 
 export default function createKeyv() {
-	const keyv = new Keyv();
+	const keyv = new Keyv(process.env.REDIS_URL ?? undefined);
 
 	/* c8 ignore next 3 */
 	keyv.on("error", (error) => {
