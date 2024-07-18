b2d14cd4fde2f660fa4a5a1a658e0aa9e722efd6
Florian Metz <me@timeraa.dev>
2024-07-18 09:39:12 +0200

    style: fix centering

--- apps/website/layouts/default.vue
+++ apps/website/layouts/default.vue
@@ -8,7 +8,7 @@ useHead({
 
 <template>
 	<Header />
-	<div class="flex justify-center flex-col min-h-screen">
+	<div class="justify-center min-h-screen mx5 flex">
 		<!-- <ScriptGoogleAdsense
 			data-ad-client="ca-pub-1575460061917202"
 			data-ad-slot="9125593977"
@@ -19,7 +19,7 @@ useHead({
 				{{ $t("layout.ads.error") }}
 			</template>
 		</ScriptGoogleAdsense> -->
-		<div class="mt-5 max-w-screen-lg mx5">
+		<div class="mt-5 max-w-screen-lg">
 			<slot />
 		</div>
 	<!-- 	<ScriptGoogleAdsense
