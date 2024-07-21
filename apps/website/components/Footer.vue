4085e2033c18373f057d7ce52f27ae7451c333c1
Florian Metz <me@timeraa.dev>
2024-07-21 23:23:05 +0200

    style: make footer follow max width

--- apps/website/components/Footer.vue
+++ apps/website/components/Footer.vue
@@ -102,49 +102,53 @@ const nonce = useNonce();
 </script>
 
 <template>
-	<div class="bg-gray min-h-50 mt-20">
-		<div class="flex justify-between flex-wrap gap-5 mx-5 py-5">
-			<div>
-				<h1 class="font-bold font-size-4.5 pb2">
-					{{ t('footer.partners') }}
-				</h1>
-				<div class="flex flex-wrap gap-1">
-					<a
-						v-for="partner in partners"
-						:key="partner.href"
-						class="block transition-opacity w-8 max-h-8 opacity-50 hover:opacity-100"
-						:href="partner.href"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						<NuxtImg :src="partner.image" :alt="partner.label" height="32px" width="32px" :nonce="nonce" />
-					</a>
+	<div class="mt-10">
+		<div class="min-h-50 bg-gray">
+			<div class="max-w-screen-lg mx-auto">
+				<div class="flex justify-between flex-wrap gap-5 mx-5 py-5">
+					<div>
+						<h1 class="font-bold font-size-4.5 pb2">
+							{{ t('footer.partners') }}
+						</h1>
+						<div class="flex flex-wrap gap-1">
+							<a
+								v-for="partner in partners"
+								:key="partner.href"
+								class="block transition-opacity w-8 max-h-8 opacity-50 hover:opacity-100"
+								:href="partner.href"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								<NuxtImg :src="partner.image" :alt="partner.label" height="32px" width="32px" :nonce="nonce" />
+							</a>
+						</div>
+					</div>
+					<div v-for="section in linkSections" :key="section.title">
+						<h1 class="font-bold font-size-4.5 pb2">
+							{{ section.title }}
+						</h1>
+						<ol>
+							<li v-for="link in section.links" :key="link.label" class="mb-2">
+								<a :href="link.href" target="_blank" class="flex gap-1 items-center color-#878b99 hover:c-light-9">
+									<FAIcon v-if="link.icon" class="h-5 w-5" :icon="link.icon" />
+									<span>{{ link.label }}</span>
+								</a>
+							</li>
+						</ol>
+					</div>
 				</div>
-			</div>
-			<div v-for="section in linkSections" :key="section.title">
-				<h1 class="font-bold font-size-4.5 pb2">
-					{{ section.title }}
-				</h1>
-				<ol>
-					<li v-for="link in section.links" :key="link.label" class="mb-2">
-						<a :href="link.href" target="_blank" class="flex gap-1 items-center color-#878b99 hover:c-light-9">
-							<FAIcon v-if="link.icon" class="h-5 w-5" :icon="link.icon" />
-							<span>{{ link.label }}</span>
-						</a>
-					</li>
-				</ol>
-			</div>
-		</div>
-		<div class="justify-center text-center mt-5 pb-5">
-			<span class="items-center inline-flex gap-2">
-				{{ t('footer.withLoveBy') }}
-				<FAIcon class="h-4 w-4 c-red" icon="fa-solid fa-heart" />
-				{{ t('footer.by') }}
-				<a href="https://recodive.com" target="_blank">Recodive</a>
-			</span>
-			<br>
-			<div class="mt2 text-xs color-light-9">
-				{{ t('footer.copyright', { year: "2018", currentYear: new Date().getFullYear(), company: 'Recodive oHG.' }) }}
+				<div class="justify-center text-center mt-5 pb-5">
+					<span class="items-center inline-flex gap-2">
+						{{ t('footer.withLoveBy') }}
+						<FAIcon class="h-4 w-4 c-red" icon="fa-solid fa-heart" />
+						{{ t('footer.by') }}
+						<a href="https://recodive.com" target="_blank">Recodive</a>
+					</span>
+					<br>
+					<div class="mt2 text-xs color-light-9">
+						{{ t('footer.copyright', { year: "2018", currentYear: new Date().getFullYear(), company: 'Recodive oHG.' }) }}
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
