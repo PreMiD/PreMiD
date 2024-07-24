9c0c7f6a0c7dd88eb09bdb44ee18b3d003893aab
Florian Metz <me@timeraa.dev>
2024-07-24 02:29:46 +0200

    New translations en.ts (Norwegian) chore(Norwegian): Update en.ts translation from Crowdin

---
+++ apps/website/locales/no/en.ts
@@ -0,0 +1,283 @@
+import { defineI18nLocale } from false;
+export default defineI18nLocale(() => ({
+  layout: {
+    ads: {
+      error: false
+    }
+  },
+  component: {
+    searchBar: {
+      search: "Søk",
+      sortBy: false,
+      searchPresence: "Søk etter Presence",
+      sort: {
+        mostUsed: false,
+        alphabetical: false
+      },
+      categories: {
+        all: "Alle",
+        anime: "Anime",
+        games: "Spill",
+        music: "Musikk",
+        other: "Andre",
+        socials: "Sosiale medier",
+        videos: "Videoer og direktesendinger"
+      }
+    },
+    browserCard: {
+      wip: false,
+      support: {
+        safari: false
+      }
+    },
+    userChip: {
+      loading: false
+    },
+    storeCard: {
+      addPresence: false,
+      removePresence: false
+    },
+    donationModal: {
+      title: false,
+      description: false,
+      continue: false,
+      close: "Lukk",
+      patreon: false,
+      github: false,
+      holdTight: false
+    }
+  },
+  header: {
+    links: {
+      contributors: "Bidragsytere",
+      downloads: "Nedlastinger",
+      features: "Funksjoner",
+      store: "Butikk"
+    }
+  },
+  page: {
+    users: {
+      userPage: {
+        title: false,
+        error: {
+          title: false,
+          description: false
+        }
+      }
+    },
+    home: {
+      meta: {
+        title: false
+      },
+      title: false,
+      subtitle: false,
+      words: {
+        music: "Musikk",
+        videos: false,
+        streams: false,
+        media: false
+      },
+      description: false,
+      getStarted: "Kom i gang!",
+      sections: {
+        feature: {
+          title: false,
+          feature1: {
+            title: false,
+            description: false
+          },
+          feature2: {
+            title: false,
+            description: false
+          },
+          feature3: {
+            title: false,
+            description: false
+          },
+          feature4: {
+            title: false,
+            description: false
+          },
+          feature5: {
+            title: false,
+            description: false
+          },
+          feature6: {
+            title: false,
+            description: false
+          }
+        },
+        howItWorks: {
+          title: false,
+          step1: {
+            title: false,
+            description: false
+          },
+          step2: {
+            title: false,
+            description: false
+          },
+          step3: {
+            title: false,
+            description: false
+          },
+          step4: {
+            title: false,
+            description: false
+          }
+        },
+        callToAction: {
+          title: false,
+          description: false,
+          button: false
+        }
+      }
+    },
+    contributors: {
+      title: "Bidragsytere",
+      presenceDevelopers: false,
+      staff: "Ansatte",
+      supporters: false,
+      translators: "Oversettere",
+      avatar: {
+        tooltip: false
+      }
+    },
+    downloads: {
+      title: "Nedlastinger",
+      steps: {
+        install: false,
+        login: false,
+        add: false,
+        showoff: false
+      },
+      section: {
+        heading: {
+          title: "På tide å vise seg fram.",
+          description: "Bruk PreMid nå og vis vennene dine hva du gjør, kanskje du finner noen med samme interesser.",
+          getStarted: "Kom i gang!",
+          extension: "Utvidelse"
+        }
+      },
+      browser: {
+        your: false,
+        other: false,
+        based: false
+      },
+      mobile: {
+        title: false,
+        description: false
+      },
+      alphaAccess: {
+        title: false,
+        description: false,
+        callToAction: false
+      },
+      faq: false,
+      faqs: {
+        q1: {
+          question: false,
+          answer: false
+        },
+        q2: {
+          question: false,
+          answer: false
+        },
+        q3: {
+          question: false,
+          answer: false
+        },
+        q4: {
+          question: false,
+          answer: false
+        },
+        q5: {
+          question: false,
+          answer: false
+        },
+        q6: {
+          question: false,
+          answer: false
+        },
+        q7: {
+          question: false,
+          answer: false
+        },
+        q8: {
+          question: false,
+          answer: false
+        },
+        q9: {
+          question: false,
+          answer: false
+        }
+      }
+    },
+    store: {
+      title: "Butikk",
+      noPresence: false,
+      presence: {
+        button: {
+          reportIssue: false,
+          suggestFeature: false,
+          viewCode: false
+        },
+        title: {
+          description: "Beskrivelse",
+          information: "Informasjon"
+        },
+        informationSection: {
+          contributors: false,
+          version: false,
+          users: false,
+          tags: false,
+          supportedUrls: false
+        }
+      },
+      header: {
+        categories: "Kategorier",
+        search: "Søk etter Presence"
+      }
+    }
+  },
+  footer: {
+    partners: "Partnere",
+    followUs: false,
+    supportUs: "Støtt oss",
+    more: "Mer",
+    legal: false,
+    supportList: {
+      donate: "Doner",
+      contribute: "Bidra",
+      translate: "Oversett"
+    },
+    moreList: {
+      faq: false,
+      documentation: "Dokumentasjon",
+      status: "Status"
+    },
+    legalList: {
+      privacyPolicy: "Personvernerklæring",
+      termsOfService: false,
+      cookiePolicy: "Retningslinjer for informasjonskapsler"
+    },
+    withLoveBy: false,
+    by: false,
+    copyright: false
+  },
+  error: {
+    404: {
+      title: false,
+      message: false
+    },
+    500: {
+      title: false,
+      message: false
+    },
+    default: {
+      title: false,
+      message: false,
+      button: false
+    }
+  }
+}));
