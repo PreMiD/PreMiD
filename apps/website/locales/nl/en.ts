36ec812c40b7d415fe79507369fbffac24e2c298
Florian Metz <me@timeraa.dev>
2024-07-24 02:29:46 +0200

    New translations en.ts (Dutch) chore(Dutch): Update en.ts translation from Crowdin

---
+++ apps/website/locales/nl/en.ts
@@ -0,0 +1,283 @@
+import { defineI18nLocale } from false;
+export default defineI18nLocale(() => ({
+  layout: {
+    ads: {
+      error: false
+    }
+  },
+  component: {
+    searchBar: {
+      search: "Zoeken",
+      sortBy: false,
+      searchPresence: "Presence zoeken",
+      sort: {
+        mostUsed: false,
+        alphabetical: false
+      },
+      categories: {
+        all: "Alle",
+        anime: "Anime",
+        games: "Spellen",
+        music: "Muziek",
+        other: "Overige",
+        socials: "Sociaal",
+        videos: "Video's & streaming"
+      }
+    },
+    browserCard: {
+      wip: false,
+      support: {
+        safari: false
+      }
+    },
+    userChip: {
+      loading: false
+    },
+    storeCard: {
+      addPresence: false,
+      removePresence: false
+    },
+    donationModal: {
+      title: false,
+      description: false,
+      continue: false,
+      close: "Sluit",
+      patreon: false,
+      github: false,
+      holdTight: false
+    }
+  },
+  header: {
+    links: {
+      contributors: "Bijdragers",
+      downloads: "Downloads",
+      features: "Functies",
+      store: "Bibliotheek"
+    }
+  },
+  page: {
+    users: {
+      userPage: {
+        title: false,
+        error: {
+          title: false,
+          description: false
+        }
+      }
+    },
+    home: {
+      meta: {
+        title: "Home"
+      },
+      title: false,
+      subtitle: false,
+      words: {
+        music: "Muziek",
+        videos: "Video's",
+        streams: false,
+        media: false
+      },
+      description: false,
+      getStarted: "Aan de slag",
+      sections: {
+        feature: {
+          title: false,
+          feature1: {
+            title: false,
+            description: false
+          },
+          feature2: {
+            title: false,
+            description: false
+          },
+          feature3: {
+            title: false,
+            description: false
+          },
+          feature4: {
+            title: false,
+            description: false
+          },
+          feature5: {
+            title: false,
+            description: false
+          },
+          feature6: {
+            title: false,
+            description: false
+          }
+        },
+        howItWorks: {
+          title: false,
+          step1: {
+            title: false,
+            description: false
+          },
+          step2: {
+            title: false,
+            description: false
+          },
+          step3: {
+            title: false,
+            description: false
+          },
+          step4: {
+            title: false,
+            description: false
+          }
+        },
+        callToAction: {
+          title: false,
+          description: false,
+          button: false
+        }
+      }
+    },
+    contributors: {
+      title: "Bijdragers",
+      presenceDevelopers: "Presenceontwikkelaars",
+      staff: "Medewerkers",
+      supporters: "Supporters",
+      translators: "Vertalers",
+      avatar: {
+        tooltip: false
+      }
+    },
+    downloads: {
+      title: "Downloads",
+      steps: {
+        install: false,
+        login: false,
+        add: false,
+        showoff: false
+      },
+      section: {
+        heading: {
+          title: "Tijd om te stralen.",
+          description: "Gebruik PreMiD nu en laat aan je vrienden zien wat je aan het doen bent, misschien vind je iemand met dezelfde interesses.",
+          getStarted: "Aan de slag",
+          extension: "Extensie"
+        }
+      },
+      browser: {
+        your: false,
+        other: false,
+        based: false
+      },
+      mobile: {
+        title: false,
+        description: false
+      },
+      alphaAccess: {
+        title: false,
+        description: false,
+        callToAction: false
+      },
+      faq: false,
+      faqs: {
+        q1: {
+          question: false,
+          answer: false
+        },
+        q2: {
+          question: false,
+          answer: false
+        },
+        q3: {
+          question: false,
+          answer: false
+        },
+        q4: {
+          question: false,
+          answer: false
+        },
+        q5: {
+          question: false,
+          answer: false
+        },
+        q6: {
+          question: false,
+          answer: false
+        },
+        q7: {
+          question: false,
+          answer: false
+        },
+        q8: {
+          question: false,
+          answer: false
+        },
+        q9: {
+          question: false,
+          answer: false
+        }
+      }
+    },
+    store: {
+      title: "Bibliotheek",
+      noPresence: false,
+      presence: {
+        button: {
+          reportIssue: false,
+          suggestFeature: false,
+          viewCode: false
+        },
+        title: {
+          description: "Beschrijving",
+          information: "Informatie"
+        },
+        informationSection: {
+          contributors: false,
+          version: false,
+          users: false,
+          tags: false,
+          supportedUrls: false
+        }
+      },
+      header: {
+        categories: "Categorieën",
+        search: "Presence zoeken"
+      }
+    }
+  },
+  footer: {
+    partners: "Partners",
+    followUs: false,
+    supportUs: "Steunen",
+    more: "Meer",
+    legal: false,
+    supportList: {
+      donate: "Doneren",
+      contribute: "Bijdragen",
+      translate: "Vertalen"
+    },
+    moreList: {
+      faq: false,
+      documentation: "Documentatie",
+      status: "Status"
+    },
+    legalList: {
+      privacyPolicy: "Privacybeleid",
+      termsOfService: false,
+      cookiePolicy: "Cookiebeleid"
+    },
+    withLoveBy: false,
+    by: false,
+    copyright: false
+  },
+  error: {
+    404: {
+      title: false,
+      message: false
+    },
+    500: {
+      title: false,
+      message: false
+    },
+    default: {
+      title: false,
+      message: false,
+      button: false
+    }
+  }
+}));
