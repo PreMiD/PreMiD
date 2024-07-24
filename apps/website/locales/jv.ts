b13d921f0d4671e89bbbbadeaf7994918daf21bf
Florian Metz <me@timeraa.dev>
2024-07-24 23:06:52 +0200

    chore(Javanese): Update en.ts translation from Crowdin

---
+++ apps/website/locales/jv.ts
@@ -0,0 +1,283 @@
+import { defineI18nLocale } from false;
+export default defineI18nLocale(() => ({
+  layout: {
+    ads: {
+      error: false
+    }
+  },
+  component: {
+    searchBar: {
+      search: "Nggoleki",
+      sortBy: false,
+      searchPresence: "Goleki Presence",
+      sort: {
+        mostUsed: false,
+        alphabetical: false
+      },
+      categories: {
+        all: "Kabeh",
+        anime: "Anime",
+        games: "Game",
+        music: "Musik",
+        other: "Liyane",
+        socials: "Sosial",
+        videos: "Vidio & Stream"
+      }
+    },
+    browserCard: {
+      wip: false,
+      support: {
+        safari: false
+      }
+    },
+    userChip: {
+      loading: false
+    },
+    storeCard: {
+      addPresence: false,
+      removePresence: false
+    },
+    donationModal: {
+      title: false,
+      description: false,
+      continue: false,
+      close: "Tutup",
+      patreon: false,
+      github: false,
+      holdTight: false
+    }
+  },
+  header: {
+    links: {
+      contributors: "Kontributor",
+      downloads: "Unduhan",
+      features: "Fitur-fitur",
+      store: "Toko"
+    }
+  },
+  page: {
+    users: {
+      userPage: {
+        title: false,
+        error: {
+          title: false,
+          description: false
+        }
+      }
+    },
+    home: {
+      meta: {
+        title: "Beranda"
+      },
+      title: false,
+      subtitle: false,
+      words: {
+        music: "Musik",
+        videos: false,
+        streams: false,
+        media: false
+      },
+      description: false,
+      getStarted: "Mulai",
+      sections: {
+        feature: {
+          title: false,
+          feature1: {
+            title: false,
+            description: false
+          },
+          feature2: {
+            title: false,
+            description: false
+          },
+          feature3: {
+            title: false,
+            description: false
+          },
+          feature4: {
+            title: false,
+            description: false
+          },
+          feature5: {
+            title: false,
+            description: false
+          },
+          feature6: {
+            title: false,
+            description: false
+          }
+        },
+        howItWorks: {
+          title: false,
+          step1: {
+            title: false,
+            description: false
+          },
+          step2: {
+            title: false,
+            description: false
+          },
+          step3: {
+            title: false,
+            description: false
+          },
+          step4: {
+            title: false,
+            description: false
+          }
+        },
+        callToAction: {
+          title: false,
+          description: false,
+          button: false
+        }
+      }
+    },
+    contributors: {
+      title: "Kontributor",
+      presenceDevelopers: false,
+      staff: "Pengurus",
+      supporters: false,
+      translators: "Penerjemah",
+      avatar: {
+        tooltip: false
+      }
+    },
+    downloads: {
+      title: "Unduhan",
+      steps: {
+        install: false,
+        login: false,
+        add: false,
+        showoff: false
+      },
+      section: {
+        heading: {
+          title: "Wektune pamer.",
+          description: "Gunakake PreMiD saiki lan pamer apa sing sampeyan lakoni, bisa uga sampeyan bakal ketemu wong sing duwe minat sing padha.",
+          getStarted: "Mulai",
+          extension: "Ekstensi"
+        }
+      },
+      browser: {
+        your: false,
+        other: false,
+        based: false
+      },
+      mobile: {
+        title: false,
+        description: false
+      },
+      alphaAccess: {
+        title: false,
+        description: false,
+        callToAction: false
+      },
+      faq: false,
+      faqs: {
+        q1: {
+          question: false,
+          answer: false
+        },
+        q2: {
+          question: false,
+          answer: false
+        },
+        q3: {
+          question: false,
+          answer: false
+        },
+        q4: {
+          question: false,
+          answer: false
+        },
+        q5: {
+          question: false,
+          answer: false
+        },
+        q6: {
+          question: false,
+          answer: false
+        },
+        q7: {
+          question: false,
+          answer: false
+        },
+        q8: {
+          question: false,
+          answer: false
+        },
+        q9: {
+          question: false,
+          answer: false
+        }
+      }
+    },
+    store: {
+      title: "Toko",
+      noPresence: false,
+      presence: {
+        button: {
+          reportIssue: false,
+          suggestFeature: false,
+          viewCode: false
+        },
+        title: {
+          description: "Deskripsi",
+          information: "Informasi"
+        },
+        informationSection: {
+          contributors: false,
+          version: false,
+          users: false,
+          tags: false,
+          supportedUrls: false
+        }
+      },
+      header: {
+        categories: "Kategori",
+        search: "Goleki Presence"
+      }
+    }
+  },
+  footer: {
+    partners: "Mitra",
+    followUs: false,
+    supportUs: "Dukung kami",
+    more: "Liyane",
+    legal: false,
+    supportList: {
+      donate: "Donasi",
+      contribute: "Kontribusi",
+      translate: "Terjemahne"
+    },
+    moreList: {
+      faq: false,
+      documentation: "Dokumentasi",
+      status: "Status"
+    },
+    legalList: {
+      privacyPolicy: "Kebijakan Privasi",
+      termsOfService: false,
+      cookiePolicy: "Kebijakan Cookie"
+    },
+    withLoveBy: false,
+    by: false,
+    copyright: false
+  },
+  error: {
+    404: {
+      title: false,
+      message: false
+    },
+    500: {
+      title: false,
+      message: false
+    },
+    default: {
+      title: false,
+      message: false,
+      button: false
+    }
+  }
+}));
