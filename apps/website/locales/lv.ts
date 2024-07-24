e80d01a47c5299f9ed0beeee31b7b3cd38f24da9
Florian Metz <me@timeraa.dev>
2024-07-24 23:06:49 +0200

    chore(Latvian): Update en.ts translation from Crowdin

---
+++ apps/website/locales/lv.ts
@@ -0,0 +1,283 @@
+import { defineI18nLocale } from false;
+export default defineI18nLocale(() => ({
+  layout: {
+    ads: {
+      error: false
+    }
+  },
+  component: {
+    searchBar: {
+      search: "Meklēt",
+      sortBy: false,
+      searchPresence: "Meklēt Presence",
+      sort: {
+        mostUsed: false,
+        alphabetical: false
+      },
+      categories: {
+        all: "Viss",
+        anime: "Anime",
+        games: "Spēles",
+        music: "Mūzika",
+        other: "Cits",
+        socials: "Sociālie tīkli",
+        videos: "Video & Straumēšana"
+      }
+    },
+    browserCard: {
+      wip: false,
+      support: {
+        safari: false
+      }
+    },
+    userChip: {
+      loading: false
+    },
+    storeCard: {
+      addPresence: false,
+      removePresence: false
+    },
+    donationModal: {
+      title: false,
+      description: false,
+      continue: false,
+      close: "Aizvērt",
+      patreon: false,
+      github: false,
+      holdTight: false
+    }
+  },
+  header: {
+    links: {
+      contributors: "Ieguldītāji",
+      downloads: "Lejupielādes",
+      features: "Funkcijas",
+      store: "Veikals"
+    }
+  },
+  page: {
+    users: {
+      userPage: {
+        title: false,
+        error: {
+          title: false,
+          description: false
+        }
+      }
+    },
+    home: {
+      meta: {
+        title: "Māja"
+      },
+      title: false,
+      subtitle: false,
+      words: {
+        music: "Mūzika",
+        videos: false,
+        streams: false,
+        media: false
+      },
+      description: false,
+      getStarted: "Sāc",
+      sections: {
+        feature: {
+          title: false,
+          feature1: {
+            title: false,
+            description: false
+          },
+          feature2: {
+            title: false,
+            description: false
+          },
+          feature3: {
+            title: false,
+            description: false
+          },
+          feature4: {
+            title: false,
+            description: false
+          },
+          feature5: {
+            title: false,
+            description: false
+          },
+          feature6: {
+            title: false,
+            description: false
+          }
+        },
+        howItWorks: {
+          title: false,
+          step1: {
+            title: false,
+            description: false
+          },
+          step2: {
+            title: false,
+            description: false
+          },
+          step3: {
+            title: false,
+            description: false
+          },
+          step4: {
+            title: false,
+            description: false
+          }
+        },
+        callToAction: {
+          title: false,
+          description: false,
+          button: false
+        }
+      }
+    },
+    contributors: {
+      title: "Ieguldītāji",
+      presenceDevelopers: false,
+      staff: "Personāls",
+      supporters: false,
+      translators: "Tulki",
+      avatar: {
+        tooltip: false
+      }
+    },
+    downloads: {
+      title: "Lejupielādes",
+      steps: {
+        install: false,
+        login: false,
+        add: false,
+        showoff: false
+      },
+      section: {
+        heading: {
+          title: "Laiks izrādīties.",
+          description: "Izmanto PreMiD tagad, lai izrādītos draugiem, ko tu dari, varbūt tu atradīsi kādu ar līdzīgām interesēm.",
+          getStarted: "Sāc",
+          extension: "Paplašinājums"
+        }
+      },
+      browser: {
+        your: false,
+        other: false,
+        based: false
+      },
+      mobile: {
+        title: false,
+        description: false
+      },
+      alphaAccess: {
+        title: false,
+        description: false,
+        callToAction: false
+      },
+      faq: false,
+      faqs: {
+        q1: {
+          question: false,
+          answer: false
+        },
+        q2: {
+          question: false,
+          answer: false
+        },
+        q3: {
+          question: false,
+          answer: false
+        },
+        q4: {
+          question: false,
+          answer: false
+        },
+        q5: {
+          question: false,
+          answer: false
+        },
+        q6: {
+          question: false,
+          answer: false
+        },
+        q7: {
+          question: false,
+          answer: false
+        },
+        q8: {
+          question: false,
+          answer: false
+        },
+        q9: {
+          question: false,
+          answer: false
+        }
+      }
+    },
+    store: {
+      title: "Veikals",
+      noPresence: false,
+      presence: {
+        button: {
+          reportIssue: false,
+          suggestFeature: false,
+          viewCode: false
+        },
+        title: {
+          description: "Apraksts",
+          information: "Informācija"
+        },
+        informationSection: {
+          contributors: false,
+          version: false,
+          users: false,
+          tags: false,
+          supportedUrls: false
+        }
+      },
+      header: {
+        categories: "Kategorijas",
+        search: "Meklēt Presence"
+      }
+    }
+  },
+  footer: {
+    partners: "Partneri",
+    followUs: false,
+    supportUs: "Atbalsti mūs",
+    more: "Vairāk",
+    legal: false,
+    supportList: {
+      donate: false,
+      contribute: "Dod savu ieguldījumu",
+      translate: "Tulko"
+    },
+    moreList: {
+      faq: false,
+      documentation: "Dokumentācija",
+      status: "Statuss"
+    },
+    legalList: {
+      privacyPolicy: "Privātuma politika",
+      termsOfService: false,
+      cookiePolicy: "Sīkdatņu Politika"
+    },
+    withLoveBy: false,
+    by: false,
+    copyright: false
+  },
+  error: {
+    404: {
+      title: false,
+      message: false
+    },
+    500: {
+      title: false,
+      message: false
+    },
+    default: {
+      title: false,
+      message: false,
+      button: false
+    }
+  }
+}));
