55b34718b5e90e4ea0b1bba924e6a72ff7a51047
Florian Metz <me@timeraa.dev>
2024-07-24 23:06:50 +0200

    chore(Malay): Update en.ts translation from Crowdin

---
+++ apps/website/locales/ms.ts
@@ -0,0 +1,283 @@
+import { defineI18nLocale } from false;
+export default defineI18nLocale(() => ({
+  layout: {
+    ads: {
+      error: false
+    }
+  },
+  component: {
+    searchBar: {
+      search: "Cari",
+      sortBy: false,
+      searchPresence: "Cari Presence",
+      sort: {
+        mostUsed: false,
+        alphabetical: false
+      },
+      categories: {
+        all: "Semua",
+        anime: "Anime",
+        games: "Permainan",
+        music: "Muzik",
+        other: "Lain-lain",
+        socials: "Sosial",
+        videos: "Video & Strim"
+      }
+    },
+    browserCard: {
+      wip: false,
+      support: {
+        safari: false
+      }
+    },
+    userChip: {
+      loading: false
+    },
+    storeCard: {
+      addPresence: false,
+      removePresence: false
+    },
+    donationModal: {
+      title: false,
+      description: false,
+      continue: false,
+      close: "Tutup",
+      patreon: false,
+      github: false,
+      holdTight: false
+    }
+  },
+  header: {
+    links: {
+      contributors: "Penyumbang",
+      downloads: "Muat Turun",
+      features: "Ciri-ciri",
+      store: "Kedai"
+    }
+  },
+  page: {
+    users: {
+      userPage: {
+        title: false,
+        error: {
+          title: false,
+          description: false
+        }
+      }
+    },
+    home: {
+      meta: {
+        title: "Laman Utama"
+      },
+      title: false,
+      subtitle: false,
+      words: {
+        music: "Muzik",
+        videos: "Video",
+        streams: false,
+        media: false
+      },
+      description: false,
+      getStarted: "Mulakan",
+      sections: {
+        feature: {
+          title: false,
+          feature1: {
+            title: false,
+            description: false
+          },
+          feature2: {
+            title: false,
+            description: false
+          },
+          feature3: {
+            title: false,
+            description: false
+          },
+          feature4: {
+            title: false,
+            description: false
+          },
+          feature5: {
+            title: false,
+            description: false
+          },
+          feature6: {
+            title: false,
+            description: false
+          }
+        },
+        howItWorks: {
+          title: false,
+          step1: {
+            title: false,
+            description: false
+          },
+          step2: {
+            title: false,
+            description: false
+          },
+          step3: {
+            title: false,
+            description: false
+          },
+          step4: {
+            title: false,
+            description: false
+          }
+        },
+        callToAction: {
+          title: false,
+          description: false,
+          button: false
+        }
+      }
+    },
+    contributors: {
+      title: "Penyumbang",
+      presenceDevelopers: "Pembangun Presence",
+      staff: "Kakitangan",
+      supporters: "Penyokong",
+      translators: "Penterjemah",
+      avatar: {
+        tooltip: false
+      }
+    },
+    downloads: {
+      title: "Muat Turun",
+      steps: {
+        install: false,
+        login: false,
+        add: false,
+        showoff: false
+      },
+      section: {
+        heading: {
+          title: "Masa untuk bermegah.",
+          description: "Gunakan PreMiD sekarang dan tunjukkan kepada rakan apa yang anda lakukan, mungkin anda akan jumpa seseorang dengan minat yang sama.",
+          getStarted: "Mulakan",
+          extension: "Sambungan"
+        }
+      },
+      browser: {
+        your: false,
+        other: false,
+        based: false
+      },
+      mobile: {
+        title: false,
+        description: false
+      },
+      alphaAccess: {
+        title: false,
+        description: false,
+        callToAction: false
+      },
+      faq: false,
+      faqs: {
+        q1: {
+          question: false,
+          answer: false
+        },
+        q2: {
+          question: false,
+          answer: false
+        },
+        q3: {
+          question: false,
+          answer: false
+        },
+        q4: {
+          question: false,
+          answer: false
+        },
+        q5: {
+          question: false,
+          answer: false
+        },
+        q6: {
+          question: false,
+          answer: false
+        },
+        q7: {
+          question: false,
+          answer: false
+        },
+        q8: {
+          question: false,
+          answer: false
+        },
+        q9: {
+          question: false,
+          answer: false
+        }
+      }
+    },
+    store: {
+      title: "Kedai",
+      noPresence: false,
+      presence: {
+        button: {
+          reportIssue: false,
+          suggestFeature: false,
+          viewCode: false
+        },
+        title: {
+          description: "Keterangan",
+          information: "Maklumat"
+        },
+        informationSection: {
+          contributors: false,
+          version: false,
+          users: false,
+          tags: false,
+          supportedUrls: false
+        }
+      },
+      header: {
+        categories: "Kategori",
+        search: "Cari Presence"
+      }
+    }
+  },
+  footer: {
+    partners: "Rakan Kongsi",
+    followUs: false,
+    supportUs: "Sokong kami",
+    more: "Lebih lagi",
+    legal: false,
+    supportList: {
+      donate: "Derma",
+      contribute: "Sumbang",
+      translate: "Terjemah"
+    },
+    moreList: {
+      faq: false,
+      documentation: "Pendokumenan",
+      status: "Status"
+    },
+    legalList: {
+      privacyPolicy: "Polisi Privasi",
+      termsOfService: false,
+      cookiePolicy: "Polisi Kuki"
+    },
+    withLoveBy: false,
+    by: false,
+    copyright: false
+  },
+  error: {
+    404: {
+      title: false,
+      message: false
+    },
+    500: {
+      title: false,
+      message: false
+    },
+    default: {
+      title: false,
+      message: false,
+      button: false
+    }
+  }
+}));
