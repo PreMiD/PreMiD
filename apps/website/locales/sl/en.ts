9589300efefe12b3df5d95fa88db6522087a2218
Florian Metz <me@timeraa.dev>
2024-07-24 02:29:51 +0200

    New translations en.ts (Slovenian) chore(Slovenian): Update en.ts translation from Crowdin

---
+++ apps/website/locales/sl/en.ts
@@ -0,0 +1,283 @@
+import { defineI18nLocale } from false;
+export default defineI18nLocale(() => ({
+  layout: {
+    ads: {
+      error: false
+    }
+  },
+  component: {
+    searchBar: {
+      search: "Išči",
+      sortBy: false,
+      searchPresence: "Išči Dodatek",
+      sort: {
+        mostUsed: false,
+        alphabetical: false
+      },
+      categories: {
+        all: "Vse",
+        anime: "Anime",
+        games: "Igre",
+        music: "Glasba",
+        other: "Drugo",
+        socials: "Družabno",
+        videos: "Videoposnetki in prenosi v živo"
+      }
+    },
+    browserCard: {
+      wip: false,
+      support: {
+        safari: false
+      }
+    },
+    userChip: {
+      loading: false
+    },
+    storeCard: {
+      addPresence: false,
+      removePresence: false
+    },
+    donationModal: {
+      title: false,
+      description: false,
+      continue: false,
+      close: "Zapri",
+      patreon: false,
+      github: false,
+      holdTight: false
+    }
+  },
+  header: {
+    links: {
+      contributors: "Sodelavci",
+      downloads: "Prenosi",
+      features: "Funkcije",
+      store: "Trgovina"
+    }
+  },
+  page: {
+    users: {
+      userPage: {
+        title: false,
+        error: {
+          title: false,
+          description: false
+        }
+      }
+    },
+    home: {
+      meta: {
+        title: false
+      },
+      title: false,
+      subtitle: false,
+      words: {
+        music: "Glasba",
+        videos: false,
+        streams: false,
+        media: false
+      },
+      description: false,
+      getStarted: "Začnite uporabljati",
+      sections: {
+        feature: {
+          title: false,
+          feature1: {
+            title: false,
+            description: false
+          },
+          feature2: {
+            title: false,
+            description: false
+          },
+          feature3: {
+            title: false,
+            description: false
+          },
+          feature4: {
+            title: false,
+            description: false
+          },
+          feature5: {
+            title: false,
+            description: false
+          },
+          feature6: {
+            title: false,
+            description: false
+          }
+        },
+        howItWorks: {
+          title: false,
+          step1: {
+            title: false,
+            description: false
+          },
+          step2: {
+            title: false,
+            description: false
+          },
+          step3: {
+            title: false,
+            description: false
+          },
+          step4: {
+            title: false,
+            description: false
+          }
+        },
+        callToAction: {
+          title: false,
+          description: false,
+          button: false
+        }
+      }
+    },
+    contributors: {
+      title: "Sodelavci",
+      presenceDevelopers: false,
+      staff: "Osebje",
+      supporters: false,
+      translators: "Prevajalci",
+      avatar: {
+        tooltip: false
+      }
+    },
+    downloads: {
+      title: "Prenosi",
+      steps: {
+        install: false,
+        login: false,
+        add: false,
+        showoff: false
+      },
+      section: {
+        heading: {
+          title: "Čas je, da se pokažeš.",
+          description: "Začnite uporabljati PreMiD zdaj in pokažite ljudem kaj delate, mogoče boste tudi našli nekoga z enakimi interesi.",
+          getStarted: "Začnite uporabljati",
+          extension: "Razširitev"
+        }
+      },
+      browser: {
+        your: false,
+        other: false,
+        based: false
+      },
+      mobile: {
+        title: false,
+        description: false
+      },
+      alphaAccess: {
+        title: false,
+        description: false,
+        callToAction: false
+      },
+      faq: false,
+      faqs: {
+        q1: {
+          question: false,
+          answer: false
+        },
+        q2: {
+          question: false,
+          answer: false
+        },
+        q3: {
+          question: false,
+          answer: false
+        },
+        q4: {
+          question: false,
+          answer: false
+        },
+        q5: {
+          question: false,
+          answer: false
+        },
+        q6: {
+          question: false,
+          answer: false
+        },
+        q7: {
+          question: false,
+          answer: false
+        },
+        q8: {
+          question: false,
+          answer: false
+        },
+        q9: {
+          question: false,
+          answer: false
+        }
+      }
+    },
+    store: {
+      title: "Trgovina",
+      noPresence: false,
+      presence: {
+        button: {
+          reportIssue: false,
+          suggestFeature: false,
+          viewCode: false
+        },
+        title: {
+          description: "Opis",
+          information: "Informacije"
+        },
+        informationSection: {
+          contributors: false,
+          version: false,
+          users: false,
+          tags: false,
+          supportedUrls: false
+        }
+      },
+      header: {
+        categories: "Kategorije",
+        search: "Išči Dodatek"
+      }
+    }
+  },
+  footer: {
+    partners: "Partnerji",
+    followUs: false,
+    supportUs: "Podprite nas",
+    more: "Več",
+    legal: false,
+    supportList: {
+      donate: "Donirajte",
+      contribute: "Prispevajte",
+      translate: "Prevajajte"
+    },
+    moreList: {
+      faq: false,
+      documentation: "Dokumentacija",
+      status: "Status"
+    },
+    legalList: {
+      privacyPolicy: "Pravilnik o zasebnosti",
+      termsOfService: false,
+      cookiePolicy: "Pravilnik o Cookie-jih"
+    },
+    withLoveBy: false,
+    by: false,
+    copyright: false
+  },
+  error: {
+    404: {
+      title: false,
+      message: false
+    },
+    500: {
+      title: false,
+      message: false
+    },
+    default: {
+      title: false,
+      message: false,
+      button: false
+    }
+  }
+}));
