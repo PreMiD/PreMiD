ac3d9d1b4d8ac79191396a2d4b00e696542577e7
Florian Metz <me@timeraa.dev>
2024-07-24 22:57:59 +0200

    chore(Hindi): Update en.ts translation from Crowdin

---
+++ apps/website/locales/hi-in.ts
@@ -0,0 +1,283 @@
+import { defineI18nLocale } from false;
+export default defineI18nLocale(() => ({
+  layout: {
+    ads: {
+      error: false
+    }
+  },
+  component: {
+    searchBar: {
+      search: "तलाश",
+      sortBy: false,
+      searchPresence: "तलाश Presence",
+      sort: {
+        mostUsed: false,
+        alphabetical: false
+      },
+      categories: {
+        all: "सभी",
+        anime: "एनिमी",
+        games: "खेल",
+        music: "संगीत",
+        other: "अन्य",
+        socials: "सामाजिक",
+        videos: "वीडियो और स्ट्रीम"
+      }
+    },
+    browserCard: {
+      wip: false,
+      support: {
+        safari: false
+      }
+    },
+    userChip: {
+      loading: false
+    },
+    storeCard: {
+      addPresence: false,
+      removePresence: false
+    },
+    donationModal: {
+      title: false,
+      description: false,
+      continue: false,
+      close: "बंध किजिए",
+      patreon: false,
+      github: false,
+      holdTight: false
+    }
+  },
+  header: {
+    links: {
+      contributors: "योगदानकर्ता",
+      downloads: "डाउन्लोड",
+      features: "फीचर्स",
+      store: "स्टोर"
+    }
+  },
+  page: {
+    users: {
+      userPage: {
+        title: false,
+        error: {
+          title: false,
+          description: false
+        }
+      }
+    },
+    home: {
+      meta: {
+        title: "होम"
+      },
+      title: false,
+      subtitle: false,
+      words: {
+        music: "संगीत",
+        videos: "वीडियो",
+        streams: false,
+        media: false
+      },
+      description: false,
+      getStarted: "Get Started",
+      sections: {
+        feature: {
+          title: false,
+          feature1: {
+            title: false,
+            description: false
+          },
+          feature2: {
+            title: false,
+            description: false
+          },
+          feature3: {
+            title: false,
+            description: false
+          },
+          feature4: {
+            title: false,
+            description: false
+          },
+          feature5: {
+            title: false,
+            description: false
+          },
+          feature6: {
+            title: false,
+            description: false
+          }
+        },
+        howItWorks: {
+          title: false,
+          step1: {
+            title: false,
+            description: false
+          },
+          step2: {
+            title: false,
+            description: false
+          },
+          step3: {
+            title: false,
+            description: false
+          },
+          step4: {
+            title: false,
+            description: false
+          }
+        },
+        callToAction: {
+          title: false,
+          description: false,
+          button: false
+        }
+      }
+    },
+    contributors: {
+      title: "योगदानकर्ता",
+      presenceDevelopers: false,
+      staff: "कर्मचारी",
+      supporters: false,
+      translators: "अनुवादकों",
+      avatar: {
+        tooltip: false
+      }
+    },
+    downloads: {
+      title: "डाउन्लोड",
+      steps: {
+        install: false,
+        login: false,
+        add: false,
+        showoff: false
+      },
+      section: {
+        heading: {
+          title: "दिखावा करने का समय",
+          description: "अब PreMiD का उपयोग करें और अपने दोस्तों को दिखाएं कि आप क्या कर रहे हैं, हो सकता है कि आप किसी को समान रुचियों वाला पाते हों",
+          getStarted: "Get Started",
+          extension: "एक्सटेंशन"
+        }
+      },
+      browser: {
+        your: false,
+        other: false,
+        based: false
+      },
+      mobile: {
+        title: false,
+        description: false
+      },
+      alphaAccess: {
+        title: false,
+        description: false,
+        callToAction: false
+      },
+      faq: false,
+      faqs: {
+        q1: {
+          question: false,
+          answer: false
+        },
+        q2: {
+          question: false,
+          answer: false
+        },
+        q3: {
+          question: false,
+          answer: false
+        },
+        q4: {
+          question: false,
+          answer: false
+        },
+        q5: {
+          question: false,
+          answer: false
+        },
+        q6: {
+          question: false,
+          answer: false
+        },
+        q7: {
+          question: false,
+          answer: false
+        },
+        q8: {
+          question: false,
+          answer: false
+        },
+        q9: {
+          question: false,
+          answer: false
+        }
+      }
+    },
+    store: {
+      title: "स्टोर",
+      noPresence: false,
+      presence: {
+        button: {
+          reportIssue: false,
+          suggestFeature: false,
+          viewCode: false
+        },
+        title: {
+          description: "विवरण",
+          information: "जानकारी"
+        },
+        informationSection: {
+          contributors: false,
+          version: false,
+          users: false,
+          tags: false,
+          supportedUrls: false
+        }
+      },
+      header: {
+        categories: "श्रेणियाँ",
+        search: "तलाश Presence"
+      }
+    }
+  },
+  footer: {
+    partners: "साझेदार",
+    followUs: false,
+    supportUs: "हमें सहयोग दीजिये",
+    more: "अतिरिक्त",
+    legal: false,
+    supportList: {
+      donate: "दान करें",
+      contribute: "योगदान दें",
+      translate: "अनुवाद करें"
+    },
+    moreList: {
+      faq: false,
+      documentation: "प्रलेखन",
+      status: "स्टेटस"
+    },
+    legalList: {
+      privacyPolicy: "गोपनीयता नीति",
+      termsOfService: false,
+      cookiePolicy: "कूकी पॉलिसी"
+    },
+    withLoveBy: false,
+    by: false,
+    copyright: false
+  },
+  error: {
+    404: {
+      title: false,
+      message: false
+    },
+    500: {
+      title: false,
+      message: false
+    },
+    default: {
+      title: false,
+      message: false,
+      button: false
+    }
+  }
+}));
