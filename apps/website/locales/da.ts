1605c1496162fd9132bc5bd4384989534e30461d
Florian Metz <me@timeraa.dev>
2024-07-24 23:06:23 +0200

    chore(Danish): Update en.ts translation from Crowdin

---
+++ apps/website/locales/da.ts
@@ -0,0 +1,283 @@
+import { defineI18nLocale } from false;
+export default defineI18nLocale(() => ({
+  layout: {
+    ads: {
+      error: false
+    }
+  },
+  component: {
+    searchBar: {
+      search: "Søg",
+      sortBy: false,
+      searchPresence: false,
+      sort: {
+        mostUsed: false,
+        alphabetical: false
+      },
+      categories: {
+        all: "Alle",
+        anime: "Anime",
+        games: "Spil",
+        music: "Musik",
+        other: "Andet",
+        socials: "Sociale Apps",
+        videos: "Videoer & Streaming"
+      }
+    },
+    browserCard: {
+      wip: false,
+      support: {
+        safari: false
+      }
+    },
+    userChip: {
+      loading: false
+    },
+    storeCard: {
+      addPresence: false,
+      removePresence: false
+    },
+    donationModal: {
+      title: false,
+      description: false,
+      continue: false,
+      close: false,
+      patreon: false,
+      github: false,
+      holdTight: false
+    }
+  },
+  header: {
+    links: {
+      contributors: "Bidragere",
+      downloads: "Installationer",
+      features: false,
+      store: "Butik"
+    }
+  },
+  page: {
+    users: {
+      userPage: {
+        title: false,
+        error: {
+          title: false,
+          description: false
+        }
+      }
+    },
+    home: {
+      meta: {
+        title: false
+      },
+      title: false,
+      subtitle: false,
+      words: {
+        music: "Musik",
+        videos: false,
+        streams: false,
+        media: false
+      },
+      description: false,
+      getStarted: "Kom i gang",
+      sections: {
+        feature: {
+          title: false,
+          feature1: {
+            title: false,
+            description: false
+          },
+          feature2: {
+            title: false,
+            description: false
+          },
+          feature3: {
+            title: false,
+            description: false
+          },
+          feature4: {
+            title: false,
+            description: false
+          },
+          feature5: {
+            title: false,
+            description: false
+          },
+          feature6: {
+            title: false,
+            description: false
+          }
+        },
+        howItWorks: {
+          title: false,
+          step1: {
+            title: false,
+            description: false
+          },
+          step2: {
+            title: false,
+            description: false
+          },
+          step3: {
+            title: false,
+            description: false
+          },
+          step4: {
+            title: false,
+            description: false
+          }
+        },
+        callToAction: {
+          title: false,
+          description: false,
+          button: false
+        }
+      }
+    },
+    contributors: {
+      title: "Bidragere",
+      presenceDevelopers: false,
+      staff: "Personale",
+      supporters: false,
+      translators: "Oversættere",
+      avatar: {
+        tooltip: false
+      }
+    },
+    downloads: {
+      title: "Installationer",
+      steps: {
+        install: false,
+        login: false,
+        add: false,
+        showoff: false
+      },
+      section: {
+        heading: {
+          title: "Tid til at vise sig.",
+          description: "Brug PreMiD nu og vis dine venner, hvad du laver, måske finder du nogen med de samme interesser.",
+          getStarted: "Kom i gang",
+          extension: "Udvidelse"
+        }
+      },
+      browser: {
+        your: false,
+        other: false,
+        based: false
+      },
+      mobile: {
+        title: false,
+        description: false
+      },
+      alphaAccess: {
+        title: false,
+        description: false,
+        callToAction: false
+      },
+      faq: false,
+      faqs: {
+        q1: {
+          question: false,
+          answer: false
+        },
+        q2: {
+          question: false,
+          answer: false
+        },
+        q3: {
+          question: false,
+          answer: false
+        },
+        q4: {
+          question: false,
+          answer: false
+        },
+        q5: {
+          question: false,
+          answer: false
+        },
+        q6: {
+          question: false,
+          answer: false
+        },
+        q7: {
+          question: false,
+          answer: false
+        },
+        q8: {
+          question: false,
+          answer: false
+        },
+        q9: {
+          question: false,
+          answer: false
+        }
+      }
+    },
+    store: {
+      title: "Butik",
+      noPresence: false,
+      presence: {
+        button: {
+          reportIssue: false,
+          suggestFeature: false,
+          viewCode: false
+        },
+        title: {
+          description: "Beskrivelse",
+          information: "Information"
+        },
+        informationSection: {
+          contributors: false,
+          version: false,
+          users: false,
+          tags: false,
+          supportedUrls: false
+        }
+      },
+      header: {
+        categories: false,
+        search: false
+      }
+    }
+  },
+  footer: {
+    partners: "Partnere",
+    followUs: false,
+    supportUs: "Støt os",
+    more: "Mere",
+    legal: false,
+    supportList: {
+      donate: "Doner",
+      contribute: "Giv os en hjælpende hånd",
+      translate: "Oversæt"
+    },
+    moreList: {
+      faq: false,
+      documentation: "Dokumentation",
+      status: "Status"
+    },
+    legalList: {
+      privacyPolicy: false,
+      termsOfService: false,
+      cookiePolicy: false
+    },
+    withLoveBy: false,
+    by: false,
+    copyright: false
+  },
+  error: {
+    404: {
+      title: false,
+      message: false
+    },
+    500: {
+      title: false,
+      message: false
+    },
+    default: {
+      title: false,
+      message: false,
+      button: false
+    }
+  }
+}));
