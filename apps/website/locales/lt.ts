96b2910a0790894a8d9b9cb2ccf97661d7daa37a
Florian Metz <me@timeraa.dev>
2024-07-24 23:06:31 +0200

    chore(Lithuanian): Update en.ts translation from Crowdin

---
+++ apps/website/locales/lt.ts
@@ -0,0 +1,283 @@
+import { defineI18nLocale } from false;
+export default defineI18nLocale(() => ({
+  layout: {
+    ads: {
+      error: false
+    }
+  },
+  component: {
+    searchBar: {
+      search: "Paieška",
+      sortBy: false,
+      searchPresence: false,
+      sort: {
+        mostUsed: false,
+        alphabetical: false
+      },
+      categories: {
+        all: "Visi",
+        anime: "Anime",
+        games: "Žaidimai",
+        music: "Muzika",
+        other: "Kita",
+        socials: "Socialiniai",
+        videos: "Vaizdo įrašai ir tiesioginiai eteriai"
+      }
+    },
+    browserCard: {
+      wip: false,
+      support: {
+        safari: false
+      }
+    },
+    userChip: {
+      loading: false
+    },
+    storeCard: {
+      addPresence: false,
+      removePresence: false
+    },
+    donationModal: {
+      title: false,
+      description: false,
+      continue: false,
+      close: false,
+      patreon: false,
+      github: false,
+      holdTight: false
+    }
+  },
+  header: {
+    links: {
+      contributors: "Pagalbininkai",
+      downloads: "Siuntiniai",
+      features: "Ypatybės",
+      store: "Parduotuvė"
+    }
+  },
+  page: {
+    users: {
+      userPage: {
+        title: false,
+        error: {
+          title: false,
+          description: false
+        }
+      }
+    },
+    home: {
+      meta: {
+        title: false
+      },
+      title: false,
+      subtitle: false,
+      words: {
+        music: "Muzika",
+        videos: false,
+        streams: false,
+        media: false
+      },
+      description: false,
+      getStarted: "Pradėkite",
+      sections: {
+        feature: {
+          title: false,
+          feature1: {
+            title: false,
+            description: false
+          },
+          feature2: {
+            title: false,
+            description: false
+          },
+          feature3: {
+            title: false,
+            description: false
+          },
+          feature4: {
+            title: false,
+            description: false
+          },
+          feature5: {
+            title: false,
+            description: false
+          },
+          feature6: {
+            title: false,
+            description: false
+          }
+        },
+        howItWorks: {
+          title: false,
+          step1: {
+            title: false,
+            description: false
+          },
+          step2: {
+            title: false,
+            description: false
+          },
+          step3: {
+            title: false,
+            description: false
+          },
+          step4: {
+            title: false,
+            description: false
+          }
+        },
+        callToAction: {
+          title: false,
+          description: false,
+          button: false
+        }
+      }
+    },
+    contributors: {
+      title: "Pagalbininkai",
+      presenceDevelopers: false,
+      staff: "Darbuotojai",
+      supporters: false,
+      translators: "Vertėjai",
+      avatar: {
+        tooltip: false
+      }
+    },
+    downloads: {
+      title: "Siuntiniai",
+      steps: {
+        install: false,
+        login: false,
+        add: false,
+        showoff: false
+      },
+      section: {
+        heading: {
+          title: "Laikas pasirodyti.",
+          description: "Pradėkite naudotis PreMiD dabar ir parodykite kitiems kuo užsiimate, galbūt rasite ką nors su tokiais pačiais pomėgiais.",
+          getStarted: "Pradėkite",
+          extension: "Plėtinys"
+        }
+      },
+      browser: {
+        your: false,
+        other: false,
+        based: false
+      },
+      mobile: {
+        title: false,
+        description: false
+      },
+      alphaAccess: {
+        title: false,
+        description: false,
+        callToAction: false
+      },
+      faq: false,
+      faqs: {
+        q1: {
+          question: false,
+          answer: false
+        },
+        q2: {
+          question: false,
+          answer: false
+        },
+        q3: {
+          question: false,
+          answer: false
+        },
+        q4: {
+          question: false,
+          answer: false
+        },
+        q5: {
+          question: false,
+          answer: false
+        },
+        q6: {
+          question: false,
+          answer: false
+        },
+        q7: {
+          question: false,
+          answer: false
+        },
+        q8: {
+          question: false,
+          answer: false
+        },
+        q9: {
+          question: false,
+          answer: false
+        }
+      }
+    },
+    store: {
+      title: "Parduotuvė",
+      noPresence: false,
+      presence: {
+        button: {
+          reportIssue: false,
+          suggestFeature: false,
+          viewCode: false
+        },
+        title: {
+          description: "Aprašymas",
+          information: "Informacija"
+        },
+        informationSection: {
+          contributors: false,
+          version: false,
+          users: false,
+          tags: false,
+          supportedUrls: false
+        }
+      },
+      header: {
+        categories: "Kategorijos",
+        search: false
+      }
+    }
+  },
+  footer: {
+    partners: "Partneriai",
+    followUs: false,
+    supportUs: "Paremkite mus",
+    more: "Daugiau",
+    legal: false,
+    supportList: {
+      donate: "Paaukoti",
+      contribute: "Prisidėkite",
+      translate: "Išversti"
+    },
+    moreList: {
+      faq: false,
+      documentation: "Dokumentacija",
+      status: "Statusas"
+    },
+    legalList: {
+      privacyPolicy: "Privatumo politika",
+      termsOfService: false,
+      cookiePolicy: "Slapukų Naudojimo Politika"
+    },
+    withLoveBy: false,
+    by: false,
+    copyright: false
+  },
+  error: {
+    404: {
+      title: false,
+      message: false
+    },
+    500: {
+      title: false,
+      message: false
+    },
+    default: {
+      title: false,
+      message: false,
+      button: false
+    }
+  }
+}));
