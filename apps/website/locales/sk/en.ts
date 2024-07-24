57ac96abf79d82ddfc3b295b2ec8f29cc951b121
Florian Metz <me@timeraa.dev>
2024-07-24 02:15:04 +0200

    New translations en.ts (Slovak)

---
+++ apps/website/locales/sk/en.ts
@@ -0,0 +1,283 @@
+import { defineI18nLocale } from false;
+export default defineI18nLocale(() => ({
+  layout: {
+    ads: {
+      error: false
+    }
+  },
+  component: {
+    searchBar: {
+      search: "Hľadať",
+      sortBy: false,
+      searchPresence: "Vyhľadať výzor",
+      sort: {
+        mostUsed: false,
+        alphabetical: false
+      },
+      categories: {
+        all: "Všetko",
+        anime: "Anime",
+        games: "Hry",
+        music: "Hudba",
+        other: "Iné",
+        socials: "Sociálne siete",
+        videos: "Videá & Streamy"
+      }
+    },
+    browserCard: {
+      wip: false,
+      support: {
+        safari: false
+      }
+    },
+    userChip: {
+      loading: false
+    },
+    storeCard: {
+      addPresence: false,
+      removePresence: false
+    },
+    donationModal: {
+      title: false,
+      description: false,
+      continue: false,
+      close: "Zavrieť",
+      patreon: false,
+      github: false,
+      holdTight: false
+    }
+  },
+  header: {
+    links: {
+      contributors: "Prispievatelia",
+      downloads: "Stiahnuť ",
+      features: "Vlastnosti",
+      store: "Obchod"
+    }
+  },
+  page: {
+    users: {
+      userPage: {
+        title: false,
+        error: {
+          title: false,
+          description: false
+        }
+      }
+    },
+    home: {
+      meta: {
+        title: "Domov"
+      },
+      title: false,
+      subtitle: false,
+      words: {
+        music: "Hudba",
+        videos: false,
+        streams: false,
+        media: false
+      },
+      description: false,
+      getStarted: "Začíname",
+      sections: {
+        feature: {
+          title: false,
+          feature1: {
+            title: false,
+            description: false
+          },
+          feature2: {
+            title: false,
+            description: false
+          },
+          feature3: {
+            title: false,
+            description: false
+          },
+          feature4: {
+            title: false,
+            description: false
+          },
+          feature5: {
+            title: false,
+            description: false
+          },
+          feature6: {
+            title: false,
+            description: false
+          }
+        },
+        howItWorks: {
+          title: false,
+          step1: {
+            title: false,
+            description: false
+          },
+          step2: {
+            title: false,
+            description: false
+          },
+          step3: {
+            title: false,
+            description: false
+          },
+          step4: {
+            title: false,
+            description: false
+          }
+        },
+        callToAction: {
+          title: false,
+          description: false,
+          button: false
+        }
+      }
+    },
+    contributors: {
+      title: "Prispievatelia",
+      presenceDevelopers: false,
+      staff: "Jadro týmu",
+      supporters: false,
+      translators: "Prekladatelia",
+      avatar: {
+        tooltip: false
+      }
+    },
+    downloads: {
+      title: "Stiahnuť ",
+      steps: {
+        install: false,
+        login: false,
+        add: false,
+        showoff: false
+      },
+      section: {
+        heading: {
+          title: "Čas na ukázanie sa.",
+          description: "Použite PreMiD teraz a pochváľte sa svojim priateľom, čo robíte, možno nájdete niekoho s rovnakými záujmami.",
+          getStarted: "Začíname",
+          extension: "Rozšírenie"
+        }
+      },
+      browser: {
+        your: false,
+        other: false,
+        based: false
+      },
+      mobile: {
+        title: false,
+        description: false
+      },
+      alphaAccess: {
+        title: false,
+        description: false,
+        callToAction: false
+      },
+      faq: false,
+      faqs: {
+        q1: {
+          question: false,
+          answer: false
+        },
+        q2: {
+          question: false,
+          answer: false
+        },
+        q3: {
+          question: false,
+          answer: false
+        },
+        q4: {
+          question: false,
+          answer: false
+        },
+        q5: {
+          question: false,
+          answer: false
+        },
+        q6: {
+          question: false,
+          answer: false
+        },
+        q7: {
+          question: false,
+          answer: false
+        },
+        q8: {
+          question: false,
+          answer: false
+        },
+        q9: {
+          question: false,
+          answer: false
+        }
+      }
+    },
+    store: {
+      title: "Obchod",
+      noPresence: false,
+      presence: {
+        button: {
+          reportIssue: false,
+          suggestFeature: false,
+          viewCode: false
+        },
+        title: {
+          description: "Popis",
+          information: "Informácie"
+        },
+        informationSection: {
+          contributors: false,
+          version: false,
+          users: false,
+          tags: false,
+          supportedUrls: false
+        }
+      },
+      header: {
+        categories: "Kategórie",
+        search: "Vyhľadať výzor"
+      }
+    }
+  },
+  footer: {
+    partners: "Partnery",
+    followUs: false,
+    supportUs: "Podpor nás",
+    more: "Viac",
+    legal: false,
+    supportList: {
+      donate: "Prispieť",
+      contribute: "Prispieť",
+      translate: "Preložiť"
+    },
+    moreList: {
+      faq: false,
+      documentation: "Dokumenty",
+      status: "Stav"
+    },
+    legalList: {
+      privacyPolicy: false,
+      termsOfService: false,
+      cookiePolicy: false
+    },
+    withLoveBy: false,
+    by: false,
+    copyright: false
+  },
+  error: {
+    404: {
+      title: false,
+      message: false
+    },
+    500: {
+      title: false,
+      message: false
+    },
+    default: {
+      title: false,
+      message: false,
+      button: false
+    }
+  }
+}));
