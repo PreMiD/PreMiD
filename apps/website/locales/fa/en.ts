e80fd5771280ebe239f8d0ca6e73855262e9f03e
Florian Metz <me@timeraa.dev>
2024-07-24 02:29:59 +0200

    New translations en.ts (Persian) chore(Persian): Update en.ts translation from Crowdin

---
+++ apps/website/locales/fa/en.ts
@@ -0,0 +1,283 @@
+import { defineI18nLocale } from false;
+export default defineI18nLocale(() => ({
+  layout: {
+    ads: {
+      error: false
+    }
+  },
+  component: {
+    searchBar: {
+      search: "جستجو",
+      sortBy: false,
+      searchPresence: "جستجوی Presence",
+      sort: {
+        mostUsed: false,
+        alphabetical: false
+      },
+      categories: {
+        all: "همه",
+        anime: "انیمه",
+        games: "بازی‌ها",
+        music: "موسیقی",
+        other: "ديگر",
+        socials: "شبکه های اجتماعی",
+        videos: "ویدیو ها و پخش های زنده"
+      }
+    },
+    browserCard: {
+      wip: false,
+      support: {
+        safari: false
+      }
+    },
+    userChip: {
+      loading: false
+    },
+    storeCard: {
+      addPresence: false,
+      removePresence: false
+    },
+    donationModal: {
+      title: false,
+      description: false,
+      continue: false,
+      close: "بستن",
+      patreon: false,
+      github: false,
+      holdTight: false
+    }
+  },
+  header: {
+    links: {
+      contributors: "مشارکت‌کنندگان",
+      downloads: "دانلود ها",
+      features: "ویژگی‌ها",
+      store: "فروشگاه"
+    }
+  },
+  page: {
+    users: {
+      userPage: {
+        title: false,
+        error: {
+          title: false,
+          description: false
+        }
+      }
+    },
+    home: {
+      meta: {
+        title: "خانه"
+      },
+      title: false,
+      subtitle: false,
+      words: {
+        music: "موسیقی",
+        videos: false,
+        streams: false,
+        media: false
+      },
+      description: false,
+      getStarted: "شروع کنید",
+      sections: {
+        feature: {
+          title: false,
+          feature1: {
+            title: false,
+            description: false
+          },
+          feature2: {
+            title: false,
+            description: false
+          },
+          feature3: {
+            title: false,
+            description: false
+          },
+          feature4: {
+            title: false,
+            description: false
+          },
+          feature5: {
+            title: false,
+            description: false
+          },
+          feature6: {
+            title: false,
+            description: false
+          }
+        },
+        howItWorks: {
+          title: false,
+          step1: {
+            title: false,
+            description: false
+          },
+          step2: {
+            title: false,
+            description: false
+          },
+          step3: {
+            title: false,
+            description: false
+          },
+          step4: {
+            title: false,
+            description: false
+          }
+        },
+        callToAction: {
+          title: false,
+          description: false,
+          button: false
+        }
+      }
+    },
+    contributors: {
+      title: "مشارکت‌کنندگان",
+      presenceDevelopers: "توسعه دهندگان Presence",
+      staff: "کارکنان",
+      supporters: false,
+      translators: "مترجمان",
+      avatar: {
+        tooltip: false
+      }
+    },
+    downloads: {
+      title: "دانلود ها",
+      steps: {
+        install: false,
+        login: false,
+        add: false,
+        showoff: false
+      },
+      section: {
+        heading: {
+          title: "وقت نمایشه.",
+          description: "از PreMiD استفاده کنید و به دوستانتان نشان دهید که دارید چه می کنید، شاید شما هم کسی را با علایق مشابه پیدا کردید.",
+          getStarted: "شروع کنید",
+          extension: "افزونه"
+        }
+      },
+      browser: {
+        your: false,
+        other: false,
+        based: false
+      },
+      mobile: {
+        title: false,
+        description: false
+      },
+      alphaAccess: {
+        title: false,
+        description: false,
+        callToAction: false
+      },
+      faq: false,
+      faqs: {
+        q1: {
+          question: false,
+          answer: false
+        },
+        q2: {
+          question: false,
+          answer: false
+        },
+        q3: {
+          question: false,
+          answer: false
+        },
+        q4: {
+          question: false,
+          answer: false
+        },
+        q5: {
+          question: false,
+          answer: false
+        },
+        q6: {
+          question: false,
+          answer: false
+        },
+        q7: {
+          question: false,
+          answer: false
+        },
+        q8: {
+          question: false,
+          answer: false
+        },
+        q9: {
+          question: false,
+          answer: false
+        }
+      }
+    },
+    store: {
+      title: "فروشگاه",
+      noPresence: false,
+      presence: {
+        button: {
+          reportIssue: false,
+          suggestFeature: false,
+          viewCode: false
+        },
+        title: {
+          description: "شرح",
+          information: "اطلاعات"
+        },
+        informationSection: {
+          contributors: false,
+          version: false,
+          users: false,
+          tags: false,
+          supportedUrls: false
+        }
+      },
+      header: {
+        categories: "دسته‌‎بندی‎ها",
+        search: "جستجوی Presence"
+      }
+    }
+  },
+  footer: {
+    partners: "همکاران",
+    followUs: false,
+    supportUs: "از ما حمایت کنید",
+    more: "بيشتر",
+    legal: false,
+    supportList: {
+      donate: "حمایت",
+      contribute: "مشارکت",
+      translate: "ترجمه"
+    },
+    moreList: {
+      faq: false,
+      documentation: "مستندات",
+      status: "وضعیت"
+    },
+    legalList: {
+      privacyPolicy: "سیاست حفظ حریم خصوصی",
+      termsOfService: false,
+      cookiePolicy: "سیاست کوکی‌ ها"
+    },
+    withLoveBy: false,
+    by: false,
+    copyright: false
+  },
+  error: {
+    404: {
+      title: false,
+      message: false
+    },
+    500: {
+      title: false,
+      message: false
+    },
+    default: {
+      title: false,
+      message: false,
+      button: false
+    }
+  }
+}));
