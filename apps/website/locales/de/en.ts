540436cf8d668bcec6fa2b76732edb4ecd61b507
Florian Metz <me@timeraa.dev>
2024-07-24 02:31:02 +0200

    chore(German): Update en.ts translation from Crowdin

---
+++ apps/website/locales/de/en.ts
@@ -0,0 +1,283 @@
+import { defineI18nLocale } from false;
+export default defineI18nLocale(() => ({
+  layout: {
+    ads: {
+      error: false
+    }
+  },
+  component: {
+    searchBar: {
+      search: "Suchen",
+      sortBy: false,
+      searchPresence: "Presence suchen",
+      sort: {
+        mostUsed: false,
+        alphabetical: false
+      },
+      categories: {
+        all: "Alle",
+        anime: "Anime",
+        games: "Spiele",
+        music: "Musik",
+        other: "Sonstige",
+        socials: "Soziale Netzwerke",
+        videos: "Videos & Streams"
+      }
+    },
+    browserCard: {
+      wip: false,
+      support: {
+        safari: false
+      }
+    },
+    userChip: {
+      loading: false
+    },
+    storeCard: {
+      addPresence: false,
+      removePresence: false
+    },
+    donationModal: {
+      title: false,
+      description: false,
+      continue: false,
+      close: "Schließen",
+      patreon: false,
+      github: false,
+      holdTight: false
+    }
+  },
+  header: {
+    links: {
+      contributors: "Mitwirkende",
+      downloads: "Downloads",
+      features: "Funktionen",
+      store: "Store"
+    }
+  },
+  page: {
+    users: {
+      userPage: {
+        title: false,
+        error: {
+          title: false,
+          description: false
+        }
+      }
+    },
+    home: {
+      meta: {
+        title: "Startseite"
+      },
+      title: false,
+      subtitle: false,
+      words: {
+        music: "Musik",
+        videos: "Videos",
+        streams: false,
+        media: false
+      },
+      description: false,
+      getStarted: "Erste Schritte",
+      sections: {
+        feature: {
+          title: false,
+          feature1: {
+            title: false,
+            description: false
+          },
+          feature2: {
+            title: false,
+            description: false
+          },
+          feature3: {
+            title: false,
+            description: false
+          },
+          feature4: {
+            title: false,
+            description: false
+          },
+          feature5: {
+            title: false,
+            description: false
+          },
+          feature6: {
+            title: false,
+            description: false
+          }
+        },
+        howItWorks: {
+          title: false,
+          step1: {
+            title: false,
+            description: false
+          },
+          step2: {
+            title: false,
+            description: false
+          },
+          step3: {
+            title: false,
+            description: false
+          },
+          step4: {
+            title: false,
+            description: false
+          }
+        },
+        callToAction: {
+          title: false,
+          description: false,
+          button: false
+        }
+      }
+    },
+    contributors: {
+      title: "Mitwirkende",
+      presenceDevelopers: false,
+      staff: "Team",
+      supporters: "Supporter",
+      translators: "Übersetzer",
+      avatar: {
+        tooltip: false
+      }
+    },
+    downloads: {
+      title: "Downloads",
+      steps: {
+        install: false,
+        login: false,
+        add: false,
+        showoff: false
+      },
+      section: {
+        heading: {
+          title: "Zeit anzugeben.",
+          description: "Beginne mit der Nutzung von PreMiD und zeige anderen, was du machst, vielleicht findest du jemanden mit den gleichen Interessen.",
+          getStarted: "Erste Schritte",
+          extension: "Browsererweiterung"
+        }
+      },
+      browser: {
+        your: false,
+        other: false,
+        based: false
+      },
+      mobile: {
+        title: false,
+        description: false
+      },
+      alphaAccess: {
+        title: false,
+        description: false,
+        callToAction: false
+      },
+      faq: false,
+      faqs: {
+        q1: {
+          question: false,
+          answer: false
+        },
+        q2: {
+          question: false,
+          answer: false
+        },
+        q3: {
+          question: false,
+          answer: false
+        },
+        q4: {
+          question: false,
+          answer: false
+        },
+        q5: {
+          question: false,
+          answer: false
+        },
+        q6: {
+          question: false,
+          answer: false
+        },
+        q7: {
+          question: false,
+          answer: false
+        },
+        q8: {
+          question: false,
+          answer: false
+        },
+        q9: {
+          question: false,
+          answer: false
+        }
+      }
+    },
+    store: {
+      title: "Store",
+      noPresence: false,
+      presence: {
+        button: {
+          reportIssue: false,
+          suggestFeature: false,
+          viewCode: false
+        },
+        title: {
+          description: "Beschreibung",
+          information: "Informationen"
+        },
+        informationSection: {
+          contributors: false,
+          version: false,
+          users: false,
+          tags: false,
+          supportedUrls: false
+        }
+      },
+      header: {
+        categories: "Kategorien",
+        search: "Presence suchen"
+      }
+    }
+  },
+  footer: {
+    partners: "Partner",
+    followUs: false,
+    supportUs: "Unterstütze uns",
+    more: "Mehr",
+    legal: false,
+    supportList: {
+      donate: "Spenden",
+      contribute: "Mitwirken",
+      translate: "Übersetzen"
+    },
+    moreList: {
+      faq: false,
+      documentation: "Dokumentation",
+      status: "Status"
+    },
+    legalList: {
+      privacyPolicy: "Datenschutzerklärung",
+      termsOfService: false,
+      cookiePolicy: "Datenschutzerklärung"
+    },
+    withLoveBy: false,
+    by: false,
+    copyright: false
+  },
+  error: {
+    404: {
+      title: false,
+      message: false
+    },
+    500: {
+      title: false,
+      message: false
+    },
+    default: {
+      title: false,
+      message: false,
+      button: false
+    }
+  }
+}));
