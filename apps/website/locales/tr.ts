8c712f0824138071bb933910c78b40f59875a15c
Florian Metz <me@timeraa.dev>
2024-07-24 23:06:38 +0200

    chore(Turkish): Update en.ts translation from Crowdin

---
+++ apps/website/locales/tr.ts
@@ -0,0 +1,283 @@
+import { defineI18nLocale } from false;
+export default defineI18nLocale(() => ({
+  layout: {
+    ads: {
+      error: false
+    }
+  },
+  component: {
+    searchBar: {
+      search: "Ara",
+      sortBy: false,
+      searchPresence: "Servis ara",
+      sort: {
+        mostUsed: false,
+        alphabetical: false
+      },
+      categories: {
+        all: "Tümü",
+        anime: "Anime",
+        games: "Oyun",
+        music: "Müzik",
+        other: "Diğer",
+        socials: "Sosyal",
+        videos: "Video & Yayın"
+      }
+    },
+    browserCard: {
+      wip: false,
+      support: {
+        safari: false
+      }
+    },
+    userChip: {
+      loading: false
+    },
+    storeCard: {
+      addPresence: false,
+      removePresence: false
+    },
+    donationModal: {
+      title: false,
+      description: false,
+      continue: false,
+      close: "İptal",
+      patreon: false,
+      github: false,
+      holdTight: false
+    }
+  },
+  header: {
+    links: {
+      contributors: "Katkıda Bulunanlar",
+      downloads: "İndir",
+      features: "Özellikler",
+      store: "Mağaza"
+    }
+  },
+  page: {
+    users: {
+      userPage: {
+        title: false,
+        error: {
+          title: false,
+          description: false
+        }
+      }
+    },
+    home: {
+      meta: {
+        title: "Ana Sayfa"
+      },
+      title: false,
+      subtitle: false,
+      words: {
+        music: "Müzik",
+        videos: "Videolar",
+        streams: false,
+        media: false
+      },
+      description: false,
+      getStarted: "Başlayın",
+      sections: {
+        feature: {
+          title: false,
+          feature1: {
+            title: false,
+            description: false
+          },
+          feature2: {
+            title: false,
+            description: false
+          },
+          feature3: {
+            title: false,
+            description: false
+          },
+          feature4: {
+            title: false,
+            description: false
+          },
+          feature5: {
+            title: false,
+            description: false
+          },
+          feature6: {
+            title: false,
+            description: false
+          }
+        },
+        howItWorks: {
+          title: false,
+          step1: {
+            title: false,
+            description: false
+          },
+          step2: {
+            title: false,
+            description: false
+          },
+          step3: {
+            title: false,
+            description: false
+          },
+          step4: {
+            title: false,
+            description: false
+          }
+        },
+        callToAction: {
+          title: false,
+          description: false,
+          button: false
+        }
+      }
+    },
+    contributors: {
+      title: "Katkıda Bulunanlar",
+      presenceDevelopers: "Servis Geliştiricileri",
+      staff: "Ekip",
+      supporters: "Destekçiler",
+      translators: "Çevirmenler",
+      avatar: {
+        tooltip: false
+      }
+    },
+    downloads: {
+      title: "İndir",
+      steps: {
+        install: false,
+        login: false,
+        add: false,
+        showoff: false
+      },
+      section: {
+        heading: {
+          title: "Gösteri zamanı.",
+          description: "PreMiD kullanmaya başlayın ve diğerlerine ne yaptığınızı gösterin, belki aynı zevkleri paylaştığınız bir kaç arkadaş edinirsiniz.",
+          getStarted: "Başlayın",
+          extension: "Eklenti"
+        }
+      },
+      browser: {
+        your: false,
+        other: false,
+        based: false
+      },
+      mobile: {
+        title: false,
+        description: false
+      },
+      alphaAccess: {
+        title: false,
+        description: false,
+        callToAction: false
+      },
+      faq: false,
+      faqs: {
+        q1: {
+          question: false,
+          answer: false
+        },
+        q2: {
+          question: false,
+          answer: false
+        },
+        q3: {
+          question: false,
+          answer: false
+        },
+        q4: {
+          question: false,
+          answer: false
+        },
+        q5: {
+          question: false,
+          answer: false
+        },
+        q6: {
+          question: false,
+          answer: false
+        },
+        q7: {
+          question: false,
+          answer: false
+        },
+        q8: {
+          question: false,
+          answer: false
+        },
+        q9: {
+          question: false,
+          answer: false
+        }
+      }
+    },
+    store: {
+      title: "Mağaza",
+      noPresence: false,
+      presence: {
+        button: {
+          reportIssue: false,
+          suggestFeature: false,
+          viewCode: false
+        },
+        title: {
+          description: "Açıklama",
+          information: "Bilgi"
+        },
+        informationSection: {
+          contributors: false,
+          version: false,
+          users: false,
+          tags: false,
+          supportedUrls: false
+        }
+      },
+      header: {
+        categories: "Kategoriler",
+        search: "Servis ara"
+      }
+    }
+  },
+  footer: {
+    partners: "Partnerler",
+    followUs: false,
+    supportUs: "Destek ol",
+    more: "Dahası",
+    legal: false,
+    supportList: {
+      donate: "Bağış Yap",
+      contribute: "Katkıda Bulun",
+      translate: "Çeviri"
+    },
+    moreList: {
+      faq: false,
+      documentation: "Dokümantasyon",
+      status: "PreMiD Durumu"
+    },
+    legalList: {
+      privacyPolicy: "Gizlilik Politikası",
+      termsOfService: false,
+      cookiePolicy: "Çerez Politikası"
+    },
+    withLoveBy: false,
+    by: false,
+    copyright: false
+  },
+  error: {
+    404: {
+      title: false,
+      message: false
+    },
+    500: {
+      title: false,
+      message: false
+    },
+    default: {
+      title: false,
+      message: false,
+      button: false
+    }
+  }
+}));
