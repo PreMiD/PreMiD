2cee215f4d6f2602149418fc9c2ee162de8203fd
Florian Metz <me@timeraa.dev>
2024-07-24 02:31:05 +0200

    chore(Irish): Update en.ts translation from Crowdin

---
+++ apps/website/locales/ga/en.ts
@@ -0,0 +1,283 @@
+import { defineI18nLocale } from false;
+export default defineI18nLocale(() => ({
+  layout: {
+    ads: {
+      error: false
+    }
+  },
+  component: {
+    searchBar: {
+      search: "Cuardaigh",
+      sortBy: false,
+      searchPresence: "Presence Cuardaigh",
+      sort: {
+        mostUsed: false,
+        alphabetical: false
+      },
+      categories: {
+        all: "Gach",
+        anime: "Anime",
+        games: "Cluichí",
+        music: "Ceol",
+        other: "Eile",
+        socials: "Sóisialta",
+        videos: "Físeáin agus Sruthanna"
+      }
+    },
+    browserCard: {
+      wip: false,
+      support: {
+        safari: false
+      }
+    },
+    userChip: {
+      loading: false
+    },
+    storeCard: {
+      addPresence: false,
+      removePresence: false
+    },
+    donationModal: {
+      title: false,
+      description: false,
+      continue: false,
+      close: "Dún",
+      patreon: false,
+      github: false,
+      holdTight: false
+    }
+  },
+  header: {
+    links: {
+      contributors: "Rannpháirtithe",
+      downloads: "Íoslódálacha",
+      features: "Gnéithe",
+      store: "Stór"
+    }
+  },
+  page: {
+    users: {
+      userPage: {
+        title: false,
+        error: {
+          title: false,
+          description: false
+        }
+      }
+    },
+    home: {
+      meta: {
+        title: "Baile"
+      },
+      title: false,
+      subtitle: false,
+      words: {
+        music: "Ceol",
+        videos: "Físeáin",
+        streams: false,
+        media: false
+      },
+      description: false,
+      getStarted: "Tosaigh Anois",
+      sections: {
+        feature: {
+          title: false,
+          feature1: {
+            title: false,
+            description: false
+          },
+          feature2: {
+            title: false,
+            description: false
+          },
+          feature3: {
+            title: false,
+            description: false
+          },
+          feature4: {
+            title: false,
+            description: false
+          },
+          feature5: {
+            title: false,
+            description: false
+          },
+          feature6: {
+            title: false,
+            description: false
+          }
+        },
+        howItWorks: {
+          title: false,
+          step1: {
+            title: false,
+            description: false
+          },
+          step2: {
+            title: false,
+            description: false
+          },
+          step3: {
+            title: false,
+            description: false
+          },
+          step4: {
+            title: false,
+            description: false
+          }
+        },
+        callToAction: {
+          title: false,
+          description: false,
+          button: false
+        }
+      }
+    },
+    contributors: {
+      title: "Rannpháirtithe",
+      presenceDevelopers: false,
+      staff: "Foireann",
+      supporters: false,
+      translators: "Aistritheoirí",
+      avatar: {
+        tooltip: false
+      }
+    },
+    downloads: {
+      title: "Íoslódálacha",
+      steps: {
+        install: false,
+        login: false,
+        add: false,
+        showoff: false
+      },
+      section: {
+        heading: {
+          title: "Am le thaispeáint as.",
+          description: "Úsáid PreMiD anois agus taispeáin do do chairde an méid atá á dhéanamh agat, b’fhéidir go bhfaighidh tú duine leis na leasanna céanna.",
+          getStarted: "Tosaigh Anois",
+          extension: "Síneadh"
+        }
+      },
+      browser: {
+        your: false,
+        other: false,
+        based: false
+      },
+      mobile: {
+        title: false,
+        description: false
+      },
+      alphaAccess: {
+        title: false,
+        description: false,
+        callToAction: false
+      },
+      faq: false,
+      faqs: {
+        q1: {
+          question: false,
+          answer: false
+        },
+        q2: {
+          question: false,
+          answer: false
+        },
+        q3: {
+          question: false,
+          answer: false
+        },
+        q4: {
+          question: false,
+          answer: false
+        },
+        q5: {
+          question: false,
+          answer: false
+        },
+        q6: {
+          question: false,
+          answer: false
+        },
+        q7: {
+          question: false,
+          answer: false
+        },
+        q8: {
+          question: false,
+          answer: false
+        },
+        q9: {
+          question: false,
+          answer: false
+        }
+      }
+    },
+    store: {
+      title: "Stór",
+      noPresence: false,
+      presence: {
+        button: {
+          reportIssue: false,
+          suggestFeature: false,
+          viewCode: false
+        },
+        title: {
+          description: "Cur síos",
+          information: "Eolas"
+        },
+        informationSection: {
+          contributors: false,
+          version: false,
+          users: false,
+          tags: false,
+          supportedUrls: false
+        }
+      },
+      header: {
+        categories: "Catagóirí",
+        search: "Presence Cuardaigh"
+      }
+    }
+  },
+  footer: {
+    partners: "Comhpháirtithe",
+    followUs: false,
+    supportUs: "Tacaigh linn",
+    more: "Níos mó",
+    legal: false,
+    supportList: {
+      donate: "Síntiús",
+      contribute: "Cur",
+      translate: "Aistrigh"
+    },
+    moreList: {
+      faq: false,
+      documentation: "Doiciméadú",
+      status: "Stádas"
+    },
+    legalList: {
+      privacyPolicy: "Beartas Príobháideachais",
+      termsOfService: false,
+      cookiePolicy: "Beartas Fianán"
+    },
+    withLoveBy: false,
+    by: false,
+    copyright: false
+  },
+  error: {
+    404: {
+      title: false,
+      message: false
+    },
+    500: {
+      title: false,
+      message: false
+    },
+    default: {
+      title: false,
+      message: false,
+      button: false
+    }
+  }
+}));
