3190e17531045b970ac4bb6abafc86d5e89de51d
Florian Metz <me@timeraa.dev>
2024-07-24 02:15:06 +0200

    New translations en.ts (Swedish)

---
+++ apps/website/locales/sv/en.ts
@@ -0,0 +1,283 @@
+import { defineI18nLocale } from false;
+export default defineI18nLocale(() => ({
+  layout: {
+    ads: {
+      error: false
+    }
+  },
+  component: {
+    searchBar: {
+      search: "Sök",
+      sortBy: false,
+      searchPresence: "Sök Presence",
+      sort: {
+        mostUsed: false,
+        alphabetical: false
+      },
+      categories: {
+        all: "Alla",
+        anime: "Anime",
+        games: "Spel",
+        music: "Musik",
+        other: "Annat",
+        socials: "Socialt",
+        videos: "Videor & Streams"
+      }
+    },
+    browserCard: {
+      wip: false,
+      support: {
+        safari: false
+      }
+    },
+    userChip: {
+      loading: false
+    },
+    storeCard: {
+      addPresence: false,
+      removePresence: false
+    },
+    donationModal: {
+      title: false,
+      description: false,
+      continue: false,
+      close: "Stäng",
+      patreon: false,
+      github: false,
+      holdTight: false
+    }
+  },
+  header: {
+    links: {
+      contributors: "Medverkande",
+      downloads: "Hämtningar",
+      features: "Funktioner",
+      store: "Butik"
+    }
+  },
+  page: {
+    users: {
+      userPage: {
+        title: false,
+        error: {
+          title: false,
+          description: false
+        }
+      }
+    },
+    home: {
+      meta: {
+        title: false
+      },
+      title: false,
+      subtitle: false,
+      words: {
+        music: "Musik",
+        videos: false,
+        streams: false,
+        media: false
+      },
+      description: false,
+      getStarted: "Kom igång",
+      sections: {
+        feature: {
+          title: false,
+          feature1: {
+            title: false,
+            description: false
+          },
+          feature2: {
+            title: false,
+            description: false
+          },
+          feature3: {
+            title: false,
+            description: false
+          },
+          feature4: {
+            title: false,
+            description: false
+          },
+          feature5: {
+            title: false,
+            description: false
+          },
+          feature6: {
+            title: false,
+            description: false
+          }
+        },
+        howItWorks: {
+          title: false,
+          step1: {
+            title: false,
+            description: false
+          },
+          step2: {
+            title: false,
+            description: false
+          },
+          step3: {
+            title: false,
+            description: false
+          },
+          step4: {
+            title: false,
+            description: false
+          }
+        },
+        callToAction: {
+          title: false,
+          description: false,
+          button: false
+        }
+      }
+    },
+    contributors: {
+      title: "Medverkande",
+      presenceDevelopers: false,
+      staff: "Personal",
+      supporters: false,
+      translators: "Översättare",
+      avatar: {
+        tooltip: false
+      }
+    },
+    downloads: {
+      title: "Hämtningar",
+      steps: {
+        install: false,
+        login: false,
+        add: false,
+        showoff: false
+      },
+      section: {
+        heading: {
+          title: "Dags att visa upp.",
+          description: "Använd PreMiD nu och visa dina vänner vad du gör, du kanske hittar någon med samma intressen.",
+          getStarted: "Kom igång",
+          extension: "Tillägg"
+        }
+      },
+      browser: {
+        your: false,
+        other: false,
+        based: false
+      },
+      mobile: {
+        title: false,
+        description: false
+      },
+      alphaAccess: {
+        title: false,
+        description: false,
+        callToAction: false
+      },
+      faq: false,
+      faqs: {
+        q1: {
+          question: false,
+          answer: false
+        },
+        q2: {
+          question: false,
+          answer: false
+        },
+        q3: {
+          question: false,
+          answer: false
+        },
+        q4: {
+          question: false,
+          answer: false
+        },
+        q5: {
+          question: false,
+          answer: false
+        },
+        q6: {
+          question: false,
+          answer: false
+        },
+        q7: {
+          question: false,
+          answer: false
+        },
+        q8: {
+          question: false,
+          answer: false
+        },
+        q9: {
+          question: false,
+          answer: false
+        }
+      }
+    },
+    store: {
+      title: "Butik",
+      noPresence: false,
+      presence: {
+        button: {
+          reportIssue: false,
+          suggestFeature: false,
+          viewCode: false
+        },
+        title: {
+          description: "Beskrivning",
+          information: "Information"
+        },
+        informationSection: {
+          contributors: false,
+          version: false,
+          users: false,
+          tags: false,
+          supportedUrls: false
+        }
+      },
+      header: {
+        categories: "Kategorier",
+        search: "Sök Presence"
+      }
+    }
+  },
+  footer: {
+    partners: "Partners",
+    followUs: false,
+    supportUs: "Stöd oss",
+    more: "Mer",
+    legal: false,
+    supportList: {
+      donate: "Donera",
+      contribute: "Bidra",
+      translate: "Översätt"
+    },
+    moreList: {
+      faq: false,
+      documentation: "Dokumentation",
+      status: "Status"
+    },
+    legalList: {
+      privacyPolicy: false,
+      termsOfService: false,
+      cookiePolicy: false
+    },
+    withLoveBy: false,
+    by: false,
+    copyright: false
+  },
+  error: {
+    404: {
+      title: false,
+      message: false
+    },
+    500: {
+      title: false,
+      message: false
+    },
+    default: {
+      title: false,
+      message: false,
+      button: false
+    }
+  }
+}));
