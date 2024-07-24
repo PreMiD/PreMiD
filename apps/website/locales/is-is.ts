33a853e89f5ddcd90b672bfb443de71b2bdc9e91
Florian Metz <me@timeraa.dev>
2024-07-24 23:06:42 +0200

    chore(Icelandic): Update en.ts translation from Crowdin

---
+++ apps/website/locales/is-is.ts
@@ -0,0 +1,283 @@
+import { defineI18nLocale } from false;
+export default defineI18nLocale(() => ({
+  layout: {
+    ads: {
+      error: false
+    }
+  },
+  component: {
+    searchBar: {
+      search: "Leita",
+      sortBy: false,
+      searchPresence: "Leitaðu Presence",
+      sort: {
+        mostUsed: false,
+        alphabetical: false
+      },
+      categories: {
+        all: false,
+        anime: false,
+        games: "Leikir",
+        music: false,
+        other: "Annað",
+        socials: "Samskiptamiðlar",
+        videos: "Myndskeið og straumar"
+      }
+    },
+    browserCard: {
+      wip: false,
+      support: {
+        safari: false
+      }
+    },
+    userChip: {
+      loading: false
+    },
+    storeCard: {
+      addPresence: false,
+      removePresence: false
+    },
+    donationModal: {
+      title: false,
+      description: false,
+      continue: false,
+      close: false,
+      patreon: false,
+      github: false,
+      holdTight: false
+    }
+  },
+  header: {
+    links: {
+      contributors: "Þátttakendur",
+      downloads: "Niðurhöl",
+      features: "Eiginleikar",
+      store: "Verslun"
+    }
+  },
+  page: {
+    users: {
+      userPage: {
+        title: false,
+        error: {
+          title: false,
+          description: false
+        }
+      }
+    },
+    home: {
+      meta: {
+        title: false
+      },
+      title: false,
+      subtitle: false,
+      words: {
+        music: false,
+        videos: false,
+        streams: false,
+        media: false
+      },
+      description: false,
+      getStarted: "Byrja",
+      sections: {
+        feature: {
+          title: false,
+          feature1: {
+            title: false,
+            description: false
+          },
+          feature2: {
+            title: false,
+            description: false
+          },
+          feature3: {
+            title: false,
+            description: false
+          },
+          feature4: {
+            title: false,
+            description: false
+          },
+          feature5: {
+            title: false,
+            description: false
+          },
+          feature6: {
+            title: false,
+            description: false
+          }
+        },
+        howItWorks: {
+          title: false,
+          step1: {
+            title: false,
+            description: false
+          },
+          step2: {
+            title: false,
+            description: false
+          },
+          step3: {
+            title: false,
+            description: false
+          },
+          step4: {
+            title: false,
+            description: false
+          }
+        },
+        callToAction: {
+          title: false,
+          description: false,
+          button: false
+        }
+      }
+    },
+    contributors: {
+      title: "Þátttakendur",
+      presenceDevelopers: false,
+      staff: "Starfsmaður",
+      supporters: false,
+      translators: "Þýðingar",
+      avatar: {
+        tooltip: false
+      }
+    },
+    downloads: {
+      title: "Niðurhöl",
+      steps: {
+        install: false,
+        login: false,
+        add: false,
+        showoff: false
+      },
+      section: {
+        heading: {
+          title: "Tími til kominn að láta sjá sig.",
+          description: "Notaðu PreMiD núna og sýndu vinum þínum hvað þú ert að gera, kannski finnurðu einhvern með sömu áhugamál.",
+          getStarted: "Byrja",
+          extension: false
+        }
+      },
+      browser: {
+        your: false,
+        other: false,
+        based: false
+      },
+      mobile: {
+        title: false,
+        description: false
+      },
+      alphaAccess: {
+        title: false,
+        description: false,
+        callToAction: false
+      },
+      faq: false,
+      faqs: {
+        q1: {
+          question: false,
+          answer: false
+        },
+        q2: {
+          question: false,
+          answer: false
+        },
+        q3: {
+          question: false,
+          answer: false
+        },
+        q4: {
+          question: false,
+          answer: false
+        },
+        q5: {
+          question: false,
+          answer: false
+        },
+        q6: {
+          question: false,
+          answer: false
+        },
+        q7: {
+          question: false,
+          answer: false
+        },
+        q8: {
+          question: false,
+          answer: false
+        },
+        q9: {
+          question: false,
+          answer: false
+        }
+      }
+    },
+    store: {
+      title: "Verslun",
+      noPresence: false,
+      presence: {
+        button: {
+          reportIssue: false,
+          suggestFeature: false,
+          viewCode: false
+        },
+        title: {
+          description: "Lýsing",
+          information: "Upplýsingar"
+        },
+        informationSection: {
+          contributors: false,
+          version: false,
+          users: false,
+          tags: false,
+          supportedUrls: false
+        }
+      },
+      header: {
+        categories: "Flokkar",
+        search: "Leitaðu Presence"
+      }
+    }
+  },
+  footer: {
+    partners: "Samstarfsaðilar",
+    followUs: false,
+    supportUs: "Sýndu okkur stuðning",
+    more: "Meira",
+    legal: false,
+    supportList: {
+      donate: "Styrkja",
+      contribute: "Bjóða fram",
+      translate: "Þýða"
+    },
+    moreList: {
+      faq: false,
+      documentation: "Skjal",
+      status: "Staða"
+    },
+    legalList: {
+      privacyPolicy: false,
+      termsOfService: false,
+      cookiePolicy: "Vafraköku Upplýsingar"
+    },
+    withLoveBy: false,
+    by: false,
+    copyright: false
+  },
+  error: {
+    404: {
+      title: false,
+      message: false
+    },
+    500: {
+      title: false,
+      message: false
+    },
+    default: {
+      title: false,
+      message: false,
+      button: false
+    }
+  }
+}));
