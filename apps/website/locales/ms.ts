54ccdf954e15f874b881585164195217f1c8412b
Florian Metz <me@timeraa.dev>
2024-08-04 11:35:20 +0200

    chore(Malay): Update en.ts translation from Crowdin

--- apps/website/locales/ms.ts
+++ apps/website/locales/ms.ts
@@ -2,17 +2,17 @@ import { defineI18nLocale } from false;
 export default defineI18nLocale(() => ({
   layout: {
     ads: {
-      error: false
+      error: "Sila sokong kami dengan mematikan penyekat iklan anda."
     }
   },
   component: {
     searchBar: {
       search: "Cari",
-      sortBy: false,
+      sortBy: "Isih ikut",
       searchPresence: "Cari Presence",
       sort: {
-        mostUsed: false,
-        alphabetical: false
+        mostUsed: "Paling Banyak Digunakan",
+        alphabetical: "Urutan Abjad"
       },
       categories: {
         all: "Semua",
@@ -25,26 +25,26 @@ export default defineI18nLocale(() => ({
       }
     },
     browserCard: {
-      wip: false,
+      wip: "Sedang Diusahakan",
       support: {
-        safari: false
+        safari: "Kami sedang mengusahakan sokongan Safari, kekal bersama!"
       }
     },
     userChip: {
-      loading: false
+      loading: "Memuatkan..."
     },
     storeCard: {
-      addPresence: false,
-      removePresence: false
+      addPresence: "Tambah",
+      removePresence: "Buang"
     },
     donationModal: {
-      title: false,
-      description: false,
-      continue: false,
+      title: "Permintaan Daripada Kami...",
+      description: "Kami berharap anda akan menyukai PreMiD! Sekiranya ia membuatkan anda tersenyum, apa kata sebarkan sedikit kasih sayang kembali? Pasukan sukarela kami menumpukan jiwa raga mereka untuk membuatkannya unggul untuk anda!",
+      continue: "Teruskan",
       close: "Tutup",
-      patreon: false,
-      github: false,
-      holdTight: false
+      patreon: "Sokong di {name}",
+      github: "Taja di {name}",
+      holdTight: "Tunggu sebentar... memuatkan butang magis..."
     }
   },
   header: {
@@ -58,10 +58,10 @@ export default defineI18nLocale(() => ({
   page: {
     users: {
       userPage: {
-        title: false,
+        title: "Sumbangan Presence",
         error: {
-          title: false,
-          description: false
+          title: "Ralat",
+          description: "Kami mengalami masalah memuatkan pengguna ini... Sila cuba lagi kemudian."
         }
       }
     },
@@ -69,67 +69,67 @@ export default defineI18nLocale(() => ({
       meta: {
         title: "Laman Utama"
       },
-      title: false,
-      subtitle: false,
+      title: "Serlahkan Kehadiran Dalam Talian Anda Dengan PreMiD",
+      subtitle: "Tunjukkan kepada rakan-rakan anda, {word} apa yang anda nikmati.",
       words: {
         music: "Muzik",
         videos: "Video",
-        streams: false,
-        media: false
+        streams: "Strim",
+        media: "Media"
       },
-      description: false,
+      description: "PreMiD ialah suatu alat mudah, berkuasa yang membolehkan anda kongsikan aktiviti media semasa anda merentasi pelbagai platform seperti YouTube, Disney+, Netflix, dan banyak lagi. Kekal berhubung dan biarkan rakan-rakan anda lihat apa yang anda lakukan pada masa nyata.",
       getStarted: "Mulakan",
       sections: {
         feature: {
-          title: false,
+          title: "Kenapa Anda Akan Suka PreMiD",
           feature1: {
-            title: false,
-            description: false
+            title: "Kawalan Privasi",
+            description: "Ambil alih tetapan privasi anda dan tentukan aktiviti apa yang anda kongsikan dengan orang lain. Data anda, peraturan anda."
           },
           feature2: {
-            title: false,
-            description: false
+            title: "Pacuan Komuniti",
+            description: "Alami sokongan yang tidak ada bandingannya untuk banyak platform, dipacu oleh komuniti yang bersemangat dan berdedikasi."
           },
           feature3: {
-            title: false,
-            description: false
+            title: "Tetapan Boleh Suai",
+            description: "Sesuaikan pengalaman PreMiD anda dengan pilihan pengubahsuaian yang meluas agar sesuai dengan keutamaan dan keperluan anda."
           },
           feature4: {
-            title: false,
-            description: false
+            title: "Tetapan Mudah",
+            description: "Mulakan dengan PreMiD dengan segera. Proses penetapan kami yang jelas dan mudah memastikan permulaan yang tidak sukar."
           },
           feature5: {
-            title: false,
-            description: false
+            title: "Patuh ToS Discord",
+            description: "Mematuhi Syarat Perkhidmatan Discord sepenuhnya dengan mengguna pakai titik hujung rasmi yang disediakan oleh Discord."
           },
           feature6: {
-            title: false,
-            description: false
+            title: "Ciri-ciri Akan Datang",
+            description: "Nantikan ciri-ciri baharu yang menyeronokkan dan penambahbaikan yang akan meningkatkan pengalaman PreMiD anda dengan lebih lanjut."
           }
         },
         howItWorks: {
-          title: false,
+          title: "Cara Ia Berfungsi",
           step1: {
-            title: false,
-            description: false
+            title: "Pasangkan Sambungan",
+            description: "Tambahkan PreMiD ke pelayar anda."
           },
           step2: {
-            title: false,
-            description: false
+            title: "Log Masuk Dengan Discord",
+            description: "Sambungkan PreMiD dengan akaun Discord anda."
           },
           step3: {
-            title: false,
-            description: false
+            title: "Tambah Perkhidmatan",
+            description: "Pilih perkhidmatan yang anda ingin paparkan, contohnya YouTube, Disney+, dan banyak lagi."
           },
           step4: {
-            title: false,
-            description: false
+            title: "Nikmati",
+            description: "Kongsikan aktiviti anda dan nikmatilah PreMiD."
           }
         },
         callToAction: {
-          title: false,
-          description: false,
-          button: false
+          title: "Bersedia Untuk Bermula?",
+          description: "Sertailah {count} pengguna yang sudah pun menyayangi PreMiD.",
+          button: "Mula Sekarang"
         }
       }
     },
@@ -140,98 +140,98 @@ export default defineI18nLocale(() => ({
       supporters: "Penyokong",
       translators: "Penterjemah",
       avatar: {
-        tooltip: false
+        tooltip: "Klik untuk salin avatar {name}"
       }
     },
     downloads: {
       title: "Muat Turun",
       steps: {
-        install: false,
-        login: false,
-        add: false,
-        showoff: false
+        install: "Pasang Sambungan",
+        login: "Log masuk dengan Discord",
+        add: "Tambah Presence",
+        showoff: "Tunjukkan aktiviti anda!"
       },
       section: {
         heading: {
           title: "Masa untuk bermegah.",
-          description: "Gunakan PreMiD sekarang dan tunjukkan kepada rakan apa yang anda lakukan, mungkin anda akan jumpa seseorang dengan minat yang sama.",
+          description: "Gunakan PreMiD sekarang dan tunjukkan kepada rakan-rakan anda apa yang anda lakukan, mungkin anda akan jumpa seseorang dengan minat yang sama.",
           getStarted: "Mulakan",
           extension: "Sambungan"
         }
       },
       browser: {
-        your: false,
-        other: false,
-        based: false
+        your: "Pelayar anda",
+        other: "Pelayar lain",
+        based: "berasaskan {browser}"
       },
       mobile: {
-        title: false,
-        description: false
+        title: "Berita buruk!",
+        description: "Harap maaf, PreMiD tidak tersedia untuk peranti mudah alih!"
       },
       alphaAccess: {
-        title: false,
-        description: false,
-        callToAction: false
+        title: "Buka Capaian Alfa Eksklusif!",
+        description: "Jejak masuk ke masa hadapan PreMiD dengan menjadi seorang Patron atau menaja kami di GitHub. Sokongan anda bukan sahaja mampu menggerakkan pembangunan kami tetapi turut memberikan anda capaian awal ke ciri-ciri yang paling membawa pembaharuan yang kami sedang ciptakan. Alami apa yang termaju yang PreMiD mampu tawarkan dan pengaruhilah trajektorinya dengan maklum balas anda. Ini bukan sahaja tentang menjadi yang pertama—ini tentang menjadi sebahagian daripada sesuatu yang lebih besar.",
+        callToAction: "Ketahui Lebih Lanjut & Sertai Pembaharuan"
       },
-      faq: false,
+      faq: "Soalan Lazim",
       faqs: {
         q1: {
-          question: false,
-          answer: false
+          question: "Apakah itu PreMiD?",
+          answer: "PreMiD ialah utiliti yang mudah, mampu ditatarajah yang membolehkan anda menunjukkan apa anda buat di sesawang ke dalam status aktiviti di Discord anda."
         },
         q2: {
-          question: false,
-          answer: false
+          question: "Bagaimanakah cara untuk menggunakan PreMiD?",
+          answer: "Anda boleh gunakan PreMiD dengan memasang sambungannya dan mengelog masuk dengan akaun Discord anda. Setelah anda log masuk, anda boleh tambah Presence ke profil anda dan tunjukkannya kepada rakan-rakan anda."
         },
         q3: {
-          question: false,
-          answer: false
+          question: "Adakah PreMiD melanggar ToS Discord?",
+          answer: "Tidak, PreMiD tidak melanggar ToS Discord. PreMiD menggunakan API Discord (termasuk titik hujung API berpagar yang disediakan oleh Discord) untuk menetapkan aktiviti anda. Ini bermakna PreMiD mematuhi ToS Discord dengan sepenuhnya."
         },
         q4: {
-          question: false,
-          answer: false
+          question: "Perkhidmatan apakah yang PreMiD sokong?",
+          answer: "PreMiD menyokong banyak perkhidmatan yang berlainan termasuk YouTube, Twitch, dan Netflix. Senarai perkhidmatan yang disokong sentiasa berkembang. Anda boleh lihat senarai penuh Presence di halaman kedai kami."
         },
         q5: {
-          question: false,
-          answer: false
+          question: "Bagaimanakah cara untuk menyumbang ke PreMiD?",
+          answer: "Anda boleh menyumbang ke PreMiD dengan menyertai komuniti kami di GitHub. Anda boleh membantu dengan melaporkan masalah, mencadangkan ciri, atau menyumbang kod."
         },
         q6: {
-          question: false,
-          answer: false
+          question: "Adakah PreMiD percuma untuk digunakan?",
+          answer: "Ya, PreMiD percuma untuk digunakan. Namun begitu, kami ada menerima derma melalui Patreon dan GitHub Sponsors untuk bantu menyokong pembangunan projek."
         },
         q7: {
-          question: false,
-          answer: false
+          question: "Apa yang patut saya lakukan jika saya hadapi masalah dengan PreMiD?",
+          answer: "Jika anda menghadapi sebarang masalah dengan PreMiD, anda boleh sertai pelayan Discord kami untuk sokongan. Kami juga ada panduan penyelesaian masalah di pendokumenan kami."
         },
         q8: {
-          question: false,
-          answer: false
+          question: "PreMiD tidak menyokong xyz, bolehkah anda menambahnya?",
+          answer: "Apa yang kami panggil sebagai Presence ini dipacu oleh komuniti, kami tidak ada sumber untuk menambah setiap satu platform. Namun begitu, anda boleh menambah Presence anda sendiri dengan mengikuti arahan di pendokumenan kami."
         },
         q9: {
-          question: false,
-          answer: false
+          question: "Berapa kerapkah PreMiD dikemas kini?",
+          answer: "Kami projek kecil yang dipacu oleh orang sukarela, kami mempunyai matlamat untuk mengemas kini PreMiD sekerap yang boleh tetapi kami tidak boleh janji yang kami akan sentiasa berjaya bertindak pantas."
         }
       }
     },
     store: {
       title: "Kedai",
-      noPresence: false,
+      noPresence: "Tiada Presence yang padan dengan carian anda...",
       presence: {
         button: {
-          reportIssue: false,
-          suggestFeature: false,
-          viewCode: false
+          reportIssue: "Laporkan Masalah",
+          suggestFeature: "Cadang Suatu Ciri",
+          viewCode: "Lihat Kod"
         },
         title: {
           description: "Keterangan",
           information: "Maklumat"
         },
         informationSection: {
-          contributors: false,
-          version: false,
-          users: false,
-          tags: false,
-          supportedUrls: false
+          contributors: "Penyumbang:",
+          version: "Versi: {version}",
+          users: "Pengguna: {users}",
+          tags: "Tag:",
+          supportedUrls: "URL disokong:"
         }
       },
       header: {
@@ -242,42 +242,42 @@ export default defineI18nLocale(() => ({
   },
   footer: {
     partners: "Rakan Kongsi",
-    followUs: false,
+    followUs: "Ikuti kami",
     supportUs: "Sokong kami",
     more: "Lebih lagi",
-    legal: false,
+    legal: "Undang-undang",
     supportList: {
       donate: "Derma",
       contribute: "Sumbang",
       translate: "Terjemah"
     },
     moreList: {
-      faq: false,
+      faq: "Soalan Lazim",
       documentation: "Pendokumenan",
       status: "Status"
     },
     legalList: {
       privacyPolicy: "Polisi Privasi",
-      termsOfService: false,
+      termsOfService: "Syarat Perkhidmatan",
       cookiePolicy: "Polisi Kuki"
     },
-    withLoveBy: false,
-    by: false,
-    copyright: false
+    withLoveBy: "Dengan",
+    by: "oleh",
+    copyright: "© {year}-{currentYear} {company} Hak cipta terpelihara."
   },
   error: {
     404: {
       title: false,
-      message: false
+      message: "Halaman yang anda cari tidak wujud."
     },
     500: {
       title: false,
-      message: false
+      message: "Ada sesuatu ralat terjadi di pihak kami."
     },
     default: {
-      title: false,
-      message: false,
-      button: false
+      title: "Ralat",
+      message: "Ada sesuatu ralat terjadi di pihak kami.",
+      button: "Kembali"
     }
   }
 }));
