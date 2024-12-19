82dd8cdd505c297abb4774fbf7e25d6348094e3f
Florian Metz <me@timeraa.dev>
2024-12-19 19:39:51 +0100

    chore(German): Update en.ts translation from Crowdin

--- apps/website/locales/de.ts
+++ apps/website/locales/de.ts
@@ -2,17 +2,17 @@ import { defineI18nLocale } from false;
 export default defineI18nLocale(() => ({
   layout: {
     ads: {
-      error: false
+      error: "Bitte unterstützen Sie uns, indem Sie Ihren Werbeblocker deaktivieren."
     }
   },
   component: {
     searchBar: {
       search: "Suchen",
-      sortBy: false,
+      sortBy: "Sortierung",
       searchPresence: "Presence suchen",
       sort: {
-        mostUsed: false,
-        alphabetical: false
+        mostUsed: "Am häufigsten verwendet",
+        alphabetical: "Alphabetisch"
       },
       categories: {
         all: "Alle",
@@ -25,26 +25,26 @@ export default defineI18nLocale(() => ({
       }
     },
     browserCard: {
-      wip: false,
+      wip: "Noch in Arbeit",
       support: {
-        safari: false
+        safari: "Wir arbeiten an der Unterstützung von Safari, bleiben Sie dran!"
       }
     },
     userChip: {
-      loading: false
+      loading: "Wird geladen..."
     },
     storeCard: {
-      addPresence: false,
-      removePresence: false
+      addPresence: "Hinzufügen",
+      removePresence: "Entfernen"
     },
     donationModal: {
-      title: false,
-      description: false,
-      continue: false,
+      title: "Ein kleiner Gefallen...",
+      description: "Wir hoffen, dass du PreMiD liebst! Wenn es ein Lächeln ins Gesicht bringt, warum nicht etwas Liebe wieder verbreiten? Unser Team von Freiwilligen gibt ihr Bestes nur für dich!",
+      continue: "Weiter",
       close: "Schließen",
-      patreon: false,
+      patreon: "Unterstützt uns auf {name}",
       github: false,
-      holdTight: false
+      holdTight: "Warte... lade den magischen Button..."
     }
   },
   header: {
@@ -60,8 +60,8 @@ export default defineI18nLocale(() => ({
       userPage: {
         title: false,
         error: {
-          title: false,
-          description: false
+          title: "Fehler",
+          description: "Wir haben Probleme beim Laden dieses Benutzers... Bitte versuchen Sie es später erneut."
         }
       }
     },
@@ -69,85 +69,85 @@ export default defineI18nLocale(() => ({
       meta: {
         title: "Startseite"
       },
-      title: false,
+      title: "Verbessern Sie Ihre Online-Darstellung mit PreMiD",
       subtitle: false,
       words: {
         music: "Musik",
         videos: "Videos",
-        streams: false,
-        media: false
+        streams: "Streams",
+        media: "Medien"
       },
-      description: false,
+      description: "PreMiD ist ein einfaches, leistungsstarkes Tool, mit dem Sie Ihre aktuelle Medienaktivität auf verschiedenen Plattformen wie YouTube, Disney+, Netflix und Anderen teilen können. Bleiben Sie verbunden und lassen Sie Ihre Freunde in Echtzeit sehen, was Sie tun.",
       getStarted: "Erste Schritte",
       sections: {
         feature: {
-          title: false,
+          title: "Warum du PreMiD liebst",
           feature1: {
-            title: false,
-            description: false
+            title: "Datenschutz",
+            description: "Ändern Sie Ihre Datenschutzeinstellungen und entscheiden Sie, welche Aktivitäten Sie mit anderen teilen. Ihre Daten, Ihre Regeln."
           },
           feature2: {
-            title: false,
-            description: false
+            title: "Community-basiert",
+            description: "Erleben Sie beispiellose Unterstützung für eine Vielzahl von Plattformen, angetrieben von einer leidenschaftlichen und engagierten Community."
           },
           feature3: {
-            title: false,
-            description: false
+            title: "Anpassbare Einstellungen",
+            description: "Passen Sie PreMiD Erlebnis mit umfangreichen Optionen nach Ihren Vorlieben und Bedürfnissen an."
           },
           feature4: {
-            title: false,
-            description: false
+            title: "Einfache Einrichtung",
+            description: "Mit unserem unkomplizierten Setup-Prozess können Sie schnell und einfach mit PreMiD starten."
           },
           feature5: {
-            title: false,
-            description: false
+            title: "Kompatibel mit Discord Nutzungsbedingungen",
+            description: "Konform mit Discord Nutzungsbedingungen, indem die offiziellen Endpunkte von Discord genutzt werden."
           },
           feature6: {
-            title: false,
-            description: false
+            title: "Zukünftige Features",
+            description: "Bleibe dran für aufregende neue Funktionen und Verbesserungen, die Ihr Erlebnis mit PreMiD noch weiter verbessern werden."
           }
         },
         howItWorks: {
-          title: false,
+          title: "So funktioniert's",
           step1: {
-            title: false,
-            description: false
+            title: "Installiere die Erweiterung",
+            description: "Füge PreMiD zu deinem Browser hinzu."
           },
           step2: {
-            title: false,
-            description: false
+            title: "Mit Discord anmelden",
+            description: "Verbinde PreMiD mit deinem Discord Account."
           },
           step3: {
-            title: false,
-            description: false
+            title: "Dienst hinzufügen",
+            description: "Wählen Sie die Dienste aus, die Sie anzeigen möchten, wie YouTube, Disney+, und mehr."
           },
           step4: {
-            title: false,
-            description: false
+            title: "Viel Spaß",
+            description: "Teilen Sie Ihre Aktivitäten und genieße PreMiD."
           }
         },
         callToAction: {
-          title: false,
-          description: false,
-          button: false
+          title: "Startklar?",
+          description: "Trete den {count} Benutzern bei, die PreMiD bereits lieben.",
+          button: "Jetzt starten"
         }
       }
     },
     contributors: {
       title: "Mitwirkende",
-      presenceDevelopers: false,
+      presenceDevelopers: "Presence-Entwickler",
       staff: "Team",
       supporters: "Supporter",
       translators: "Übersetzer",
       avatar: {
-        tooltip: false
+        tooltip: "Klicke, um den Avatar von {name} zu kopieren"
       }
     },
     downloads: {
       title: "Downloads",
       steps: {
-        install: false,
-        login: false,
+        install: "Erweiterung installieren",
+        login: "Mit Discord anmelden",
         add: false,
         showoff: false
       },
@@ -219,7 +219,7 @@ export default defineI18nLocale(() => ({
       presence: {
         button: {
           reportIssue: false,
-          suggestFeature: false,
+          suggestFeature: "Schlage eine Funktion vor",
           viewCode: false
         },
         title: {
@@ -272,12 +272,12 @@ export default defineI18nLocale(() => ({
     },
     500: {
       title: false,
-      message: false
+      message: "Entschuldige, etwas ist bei uns schief gelaufen."
     },
     default: {
-      title: false,
-      message: false,
-      button: false
+      title: "Fehler",
+      message: "Entschuldige, etwas ist bei uns schief gelaufen.",
+      button: "Zurück"
     }
   }
 }));
