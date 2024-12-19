69558a96aa99ec51c72f2059335af6f7bb688290
Florian Metz <me@timeraa.dev>
2024-12-19 19:40:11 +0100

    chore(Chinese Traditional): Update en.ts translation from Crowdin

--- apps/website/locales/zh-tw.ts
+++ apps/website/locales/zh-tw.ts
@@ -2,17 +2,17 @@ import { defineI18nLocale } from false;
 export default defineI18nLocale(() => ({
   layout: {
     ads: {
-      error: false
+      error: "希望您停用廣告攔截器來支持我們。"
     }
   },
   component: {
     searchBar: {
       search: "搜尋",
-      sortBy: false,
+      sortBy: "排序",
       searchPresence: "搜尋 Presence",
       sort: {
-        mostUsed: false,
-        alphabetical: false
+        mostUsed: "最常使用",
+        alphabetical: "依字母"
       },
       categories: {
         all: "全部",
@@ -21,26 +21,26 @@ export default defineI18nLocale(() => ({
         music: "音樂",
         other: "其它",
         socials: "社交",
-        videos: "視訊或直播"
+        videos: "影片或直播"
       }
     },
     browserCard: {
-      wip: false,
+      wip: "正在製作中(WIP)",
       support: {
-        safari: false
+        safari: "我們正在努力支援Safari瀏覽器中，敬請關注！"
       }
     },
     userChip: {
-      loading: false
+      loading: "讀取中…"
     },
     storeCard: {
-      addPresence: false,
-      removePresence: false
+      addPresence: "新增",
+      removePresence: "移除"
     },
     donationModal: {
       title: false,
       description: false,
-      continue: false,
+      continue: "下一步",
       close: "關閉",
       patreon: false,
       github: false,
@@ -52,7 +52,7 @@ export default defineI18nLocale(() => ({
       contributors: "貢獻者",
       downloads: "下載",
       features: "功能",
-      store: false
+      store: "商店"
     }
   },
   page: {
@@ -60,8 +60,8 @@ export default defineI18nLocale(() => ({
       userPage: {
         title: false,
         error: {
-          title: false,
-          description: false
+          title: "錯誤",
+          description: "我們在載入這個用戶時遇到問題... 請稍後再試。"
         }
       }
     },
@@ -73,12 +73,12 @@ export default defineI18nLocale(() => ({
       subtitle: false,
       words: {
         music: "音樂",
-        videos: false,
-        streams: false,
-        media: false
+        videos: "影片",
+        streams: "直播",
+        media: "媒體"
       },
       description: false,
-      getStarted: "馬上開始",
+      getStarted: "開始使用",
       sections: {
         feature: {
           title: false,
@@ -135,10 +135,10 @@ export default defineI18nLocale(() => ({
     },
     contributors: {
       title: "貢獻者",
-      presenceDevelopers: false,
+      presenceDevelopers: "狀態開發者",
       staff: "職員",
-      supporters: false,
-      translators: "翻譯員",
+      supporters: "贊助者",
+      translators: "翻譯人員",
       avatar: {
         tooltip: false
       }
@@ -146,9 +146,9 @@ export default defineI18nLocale(() => ({
     downloads: {
       title: "下載",
       steps: {
-        install: false,
-        login: false,
-        add: false,
+        install: "安裝擴充功能",
+        login: "使用 Discord 登入",
+        add: "新增狀態",
         showoff: false
       },
       section: {
@@ -156,17 +156,17 @@ export default defineI18nLocale(() => ({
           title: "是時候展示你自己了!",
           description: "現在開始使用PreMiD,向其他人展示你在做什麼,可能你會找到志同道合的朋友。 ",
           getStarted: "馬上開始",
-          extension: "擴展程序"
+          extension: "擴充功能"
         }
       },
       browser: {
-        your: false,
-        other: false,
+        your: "您的瀏覽器",
+        other: "其他瀏覽器",
         based: false
       },
       mobile: {
-        title: false,
-        description: false
+        title: "壞消息!",
+        description: "PreMiD 不支援行動裝置, 非常抱歉!"
       },
       alphaAccess: {
         title: false,
@@ -214,7 +214,7 @@ export default defineI18nLocale(() => ({
       }
     },
     store: {
-      title: false,
+      title: "商店",
       noPresence: false,
       presence: {
         button: {
@@ -258,7 +258,7 @@ export default defineI18nLocale(() => ({
     },
     legalList: {
       privacyPolicy: "隱私權政策",
-      termsOfService: false,
+      termsOfService: "服務條款",
       cookiePolicy: "Cookie 政策"
     },
     withLoveBy: false,
@@ -275,9 +275,9 @@ export default defineI18nLocale(() => ({
       message: false
     },
     default: {
-      title: false,
+      title: "錯誤",
       message: false,
-      button: false
+      button: "返回"
     }
   }
 }));
