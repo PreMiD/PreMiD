4b43e03ff01dbbc69c8f021bb33ea1e4feeed51a
Bas950 <me@bas950.com>
2024-12-19 20:33:08 +0100

    chore: remove ip lookup

--- apps/api-master/environment.d.ts
+++ apps/api-master/environment.d.ts
@@ -1,14 +1,3 @@
-declare module "ip-location-api" {
-	export function lookup(ip: string): Promise<{
-		latitude: number;
-		longitude: number;
-		country: string;
-	} | null>;
-
-	export function updateDb(options: { fields?: string[]; dataDir?: string; tmpDataDir?: string; smallMemory?: boolean; autoUpdate?: string }): Promise<void>;
-	export function reload(options: { fields?: string[]; dataDir?: string; tmpDataDir?: string; smallMemory?: boolean; autoUpdate?: string }): Promise<void>;
-}
-
 declare namespace NodeJS {
 	export interface ProcessEnv {
 		METRICS_DATABASE_URL?: string;
