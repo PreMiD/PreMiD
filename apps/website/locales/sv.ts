010dc4cfaf93fa8f89e532e15b630e3a3422689c
Florian Metz <me@timeraa.dev>
2024-12-20 04:11:17 +0100

    chore(Swedish): Update en.ts translation from Crowdin

--- apps/website/locales/sv.ts
+++ apps/website/locales/sv.ts
@@ -2,17 +2,17 @@ import { defineI18nLocale } from false;
 export default defineI18nLocale(() => ({
   layout: {
     ads: {
-      error: false
+      error: "Stöd oss genom att inaktivera din annonsblockerare."
     }
   },
   component: {
     searchBar: {
       search: "Sök",
-      sortBy: false,
+      sortBy: "Sortera efter",
       searchPresence: "Sök Presence",
       sort: {
-        mostUsed: false,
-        alphabetical: false
+        mostUsed: "Mest använda",
+        alphabetical: "Alfabetisk"
       },
       categories: {
         all: "Alla",
@@ -25,26 +25,26 @@ export default defineI18nLocale(() => ({
       }
     },
     browserCard: {
-      wip: false,
+      wip: "Arbete pågår",
       support: {
-        safari: false
+        safari: "Vi arbetar på att stödja Safari, håll ögonen öppna!"
       }
     },
     userChip: {
-      loading: false
+      loading: "Läser in..."
     },
     storeCard: {
-      addPresence: false,
-      removePresence: false
+      addPresence: "Lägg till",
+      removePresence: "Ta bort"
     },
     donationModal: {
-      title: false,
-      description: false,
-      continue: false,
+      title: "En snabb förfrågan...",
+      description: "Vi hoppas att du ska älska PreMiD! Om det ger ett leende i ditt ansikte, varför inte sprida lite kärlek tillbaka? Vårt team av volontärer lägger sina hjärtan på att göra det fantastiskt bara för dig!",
+      continue: "Fortsätt",
       close: "Stäng",
-      patreon: false,
-      github: false,
-      holdTight: false
+      patreon: "Stöd oss på {name}",
+      github: "Sponsra oss på {name}",
+      holdTight: "Håll tight... laddar den magiska knappen..."
     }
   },
   header: {
@@ -58,10 +58,10 @@ export default defineI18nLocale(() => ({
   page: {
     users: {
       userPage: {
-        title: false,
+        title: "Presence Bidragare",
         error: {
-          title: false,
-          description: false
+          title: "Fel",
+          description: "Vi har problem med att ladda denna användare... Försök igen senare."
         }
       }
     },
@@ -69,67 +69,67 @@ export default defineI18nLocale(() => ({
       meta: {
         title: "Hem"
       },
-      title: false,
-      subtitle: false,
+      title: "Förbättra din online-närvaro med PreMiD",
+      subtitle: "Visa dina vänner vad {word} du tycker om.",
       words: {
         music: "Musik",
         videos: "Videor",
-        streams: false,
-        media: false
+        streams: "Strömmar",
+        media: "Media"
       },
-      description: false,
+      description: "PreMiD är ett enkelt och kraftfullt verktyg som låter dig dela din aktuella medieaktivitet på flera plattformar som YouTube, Disney+, Netflix och mer. Håll kontakten och låt dina vänner se vad du håller på med i realtid.",
       getStarted: "Kom igång",
       sections: {
         feature: {
-          title: false,
+          title: "Varför du kommer att älska PreMiD",
           feature1: {
-            title: false,
-            description: false
+            title: "Sekretess Kontroll",
+            description: "Ta hand om dina sekretessinställningar och besluta vilka aktiviteter du delar med andra. Dina data, dina regler."
           },
           feature2: {
-            title: false,
-            description: false
+            title: "Gemenskapsdriven",
+            description: "Upplev enastående stöd för en mängd olika plattformar, som drivs av en passionerad och hängiven gemenskap."
           },
           feature3: {
-            title: false,
-            description: false
+            title: "Anpassningsbara inställningar",
+            description: "Skräddarsy din PreMiD-upplevelse med omfattande anpassningsalternativ som passar dina önskemål och behov."
           },
           feature4: {
-            title: false,
-            description: false
+            title: "Enkel installation",
+            description: "Kom igång med PreMiD på nolltid. Vår enkla installationsprocess garanterar en problemfri start."
           },
           feature5: {
-            title: false,
-            description: false
+            title: "Discord ToS kompatibel",
+            description: "Helt i överensstämmelse med Discords användarvillkor genom att använda officiella slutpunkter som tillhandahålls av Discord."
           },
           feature6: {
-            title: false,
-            description: false
+            title: "Framtida Funktioner",
+            description: "Håll ögonen öppna för nya spännande funktioner och förbättringar som kommer att förbättra din PreMiD-upplevelse ytterligare."
           }
         },
         howItWorks: {
-          title: false,
+          title: "Hur det Fungerar",
           step1: {
-            title: false,
-            description: false
+            title: "Installera Tillägget",
+            description: "Lägg till PreMiD till din webbläsare."
           },
           step2: {
-            title: false,
-            description: false
+            title: "Logga in med Discord",
+            description: "Anslut PreMiD till ditt Discord-konto."
           },
           step3: {
-            title: false,
-            description: false
+            title: "Lägg till Tjänster",
+            description: "Välj de tjänster du vill visa, som YouTube, Disney+, med mera."
           },
           step4: {
-            title: false,
-            description: false
+            title: "Njut",
+            description: "Dela din aktivitet och njut av att använda PreMiD."
           }
         },
         callToAction: {
-          title: false,
-          description: false,
-          button: false
+          title: "Redo att komma igång ?",
+          description: "Gå med i {count} användare som redan älskar PreMiD.",
+          button: "Påbörja nu"
         }
       }
     },
@@ -140,16 +140,16 @@ export default defineI18nLocale(() => ({
       supporters: "Supportrar",
       translators: "Översättare",
       avatar: {
-        tooltip: false
+        tooltip: "Klicka för att kopiera {name}s avatar"
       }
     },
     downloads: {
       title: "Hämtningar",
       steps: {
-        install: false,
-        login: false,
-        add: false,
-        showoff: false
+        install: "Installera Tillägg",
+        login: "Logga in med Discord",
+        add: "Lägg till Presence",
+        showoff: "Visa av!"
       },
       section: {
         heading: {
@@ -160,78 +160,78 @@ export default defineI18nLocale(() => ({
         }
       },
       browser: {
-        your: false,
-        other: false,
-        based: false
+        your: "Din webbläsare",
+        other: "Andra webbläsare",
+        based: "{browser} baserat"
       },
       mobile: {
-        title: false,
-        description: false
+        title: "Dåliga nyheter!",
+        description: "PreMiD är inte tillgängligt för mobila enheter, ledsen!"
       },
       alphaAccess: {
-        title: false,
-        description: false,
-        callToAction: false
+        title: "Lås upp exklusiv Alpha-åtkomst!",
+        description: "Kliv in i framtiden för PreMiD genom att bli en Patreon eller sponsra oss på GitHub. Ditt stöd driver inte bara vår utveckling utan ger dig även första tillgång till de mest innovativa funktioner vi tillverkar. Upplev den banbrytande delen av vad PreMiD kan erbjuda och påverka sin bana med din feedback. Det handlar inte bara om att vara först – det handlar om att vara en del av något större.",
+        callToAction: "Läs mer och delta i vår Innovation"
       },
-      faq: false,
+      faq: "Vanliga Frågor",
       faqs: {
         q1: {
-          question: false,
-          answer: false
+          question: "Vad är PreMiD?",
+          answer: "PreMiD är ett enkelt och konfigurerbart verktyg som tillåter dig att visa vad du gör på internet i din Discord-status."
         },
         q2: {
-          question: false,
-          answer: false
+          question: "Hur använder jag PreMiD?",
+          answer: "Du kan använda PreMiD genom att installera tillägget och logga in med ditt Discord-konto. När du är inloggad kan du lägga till närvaron till din profil och visa upp för dina vänner."
         },
         q3: {
-          question: false,
-          answer: false
+          question: "Är PreMiD mot Discords ToS?",
+          answer: "Nej, PreMiD är inte emot Discords ToS. PreMiD använder Discords API (inklusive gated API endpoints som tillhandahålls av Discord) för att ställa in din aktivitet. Detta innebär att PreMiD är i full överensstämmelse med Discords ToS."
         },
         q4: {
-          question: false,
-          answer: false
+          question: "Vilka tjänster stödjer PreMiD?",
+          answer: "PreMiD stöder många olika tjänster, inklusive YouTube, Twitch, och Netflix. Listan över stödda tjänster växer ständigt. Du kan se hela listan över Presences på vår butikssida."
         },
         q5: {
-          question: false,
-          answer: false
+          question: "Hur kan jag bidra till PreMiD?",
+          answer: "Du kan bidra till PreMiD genom att gå med i vår community på GitHub. Du kan hjälpa till genom att rapportera problem, föreslå funktioner eller bidra med kod."
         },
         q6: {
-          question: false,
-          answer: false
+          question: "Är PreMiD gratis att använda?",
+          answer: "Ja, PreMiD är gratis att använda. Vi accepterar dock donationer via Patreon och GitHub Sponsorer för att stödja utvecklingen av projektet."
         },
         q7: {
-          question: false,
-          answer: false
+          question: "Vad ska jag göra om jag stöter på ett problem med PreMiD?",
+          answer: "Om du stöter på några problem med PreMiD kan du gå med i vår Discord-server för support. Vi har också en felsökningsguide på vår dokumentation."
         },
         q8: {
-          question: false,
-          answer: false
+          question: "PreMiD stöder inte xyz, kan du lägga till det?",
+          answer: "Våra så kallade Presences är samhällsstyrda, vi har inte resurser att lägga till varenda plattform. Du kan dock lägga till din egen Presence genom att följa instruktionerna på vår dokumentation."
         },
         q9: {
-          question: false,
-          answer: false
+          question: "Hur ofta uppdateras PreMiD?",
+          answer: "Vi är ett litet volontärstyrt projekt, vi strävar efter att uppdatera PreMiD så ofta som möjligt, men vi kan inte lova att vi alltid kommer att vara på toppen av saker."
         }
       }
     },
     store: {
       title: "Butik",
-      noPresence: false,
+      noPresence: "Ingen presence matchar din sökning...",
       presence: {
         button: {
-          reportIssue: false,
-          suggestFeature: false,
-          viewCode: false
+          reportIssue: "Rapportera ett problem",
+          suggestFeature: "Föreslå en Funktion",
+          viewCode: "Visa Kod"
         },
         title: {
           description: "Beskrivning",
           information: "Information"
         },
         informationSection: {
-          contributors: false,
-          version: false,
-          users: false,
-          tags: false,
-          supportedUrls: false
+          contributors: "Bidragsgivare:",
+          version: "Version: {version}",
+          users: "Användare: {users}",
+          tags: "Taggar:",
+          supportedUrls: "Stöda URLs:"
         }
       },
       header: {
@@ -242,42 +242,42 @@ export default defineI18nLocale(() => ({
   },
   footer: {
     partners: "Partners",
-    followUs: false,
+    followUs: "Följ oss",
     supportUs: "Stöd oss",
     more: "Mer",
-    legal: false,
+    legal: "Juridiskt",
     supportList: {
       donate: "Donera",
       contribute: "Bidra",
       translate: "Översätt"
     },
     moreList: {
-      faq: false,
+      faq: "FAQ",
       documentation: "Dokumentation",
       status: "Status"
     },
     legalList: {
       privacyPolicy: "Sekretesspolicy",
-      termsOfService: false,
+      termsOfService: "Användarvillkor",
       cookiePolicy: "Cookie Policy"
     },
-    withLoveBy: false,
-    by: false,
-    copyright: false
+    withLoveBy: "Med",
+    by: "av",
+    copyright: "©️ {year}-{currentYear} {company} Alla rättigheter reserverade."
   },
   error: {
     404: {
       title: false,
-      message: false
+      message: "Sidan du letar efter finns inte."
     },
     500: {
       title: false,
-      message: false
+      message: "Någonting gick fel på vår sida."
     },
     default: {
-      title: false,
-      message: false,
-      button: false
+      title: "Fel",
+      message: "Någonting gick fel på vår sida.",
+      button: "Gå Tillbaka"
     }
   }
 }));
