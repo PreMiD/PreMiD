fb2d30e249d782a8919a427012cbda3d8fd64b16
Florian Metz <me@timeraa.dev>
2025-01-08 04:49:28 +0100

    chore(Czech): Update en.ts translation from Crowdin

--- apps/website/locales/cs.ts
+++ apps/website/locales/cs.ts
@@ -2,14 +2,14 @@ import { defineI18nLocale } from false;
 export default defineI18nLocale(() => ({
   layout: {
     ads: {
-      error: false
+      error: "Podpořte násvypnutím vašeho blokovače reklam."
     }
   },
   component: {
     searchBar: {
       search: "Hledat",
       sortBy: "Seřadit podle",
-      searchPresence: "Hledat Presence",
+      searchPresence: "Hledat Aktivity",
       sort: {
         mostUsed: "Nejpoužívanější",
         alphabetical: "Abecedně"
@@ -25,9 +25,9 @@ export default defineI18nLocale(() => ({
       }
     },
     browserCard: {
-      wip: "WIP",
+      wip: "V přípravě",
       support: {
-        safari: false
+        safari: "Pracujeme na podpoře Safari, buďte připraveni!"
       }
     },
     userChip: {
@@ -38,19 +38,19 @@ export default defineI18nLocale(() => ({
       removePresence: "Odebrat"
     },
     donationModal: {
-      title: false,
-      description: false,
+      title: "Menší příspěvek...",
+      description: "Doufáme, že máš rád PreMiD! Pokud to vnáší úsměv k tváři, proč nešířit nějakou lásku zpět? Náš tým dobrovolníků dal své srdce aby toto vytvořil právě pro vás!",
       continue: "Pokračovat",
       close: "Zavřít",
-      patreon: false,
-      github: false,
-      holdTight: false
+      patreon: "Podpoř na {name}",
+      github: "Sponzor na {name}",
+      holdTight: "Vydrž... Načítám magické tlačítko..."
     }
   },
   header: {
     links: {
       contributors: "Přispěvatelé",
-      downloads: "Stáhnout",
+      downloads: "Ke Stažení",
       features: "Funkce",
       store: "Obchod"
     }
@@ -58,10 +58,10 @@ export default defineI18nLocale(() => ({
   page: {
     users: {
       userPage: {
-        title: false,
+        title: "Přispěvatelé aktivit",
         error: {
           title: "Chyba",
-          description: false
+          description: "Máme problém s načtením tohoto uživatele... Zkuste to prosím znovu později."
         }
       }
     },
@@ -69,86 +69,86 @@ export default defineI18nLocale(() => ({
       meta: {
         title: "Domů"
       },
-      title: false,
-      subtitle: false,
+      title: "Vylepšete svou online aktivitu pomocí PreMiD",
+      subtitle: "Ukažte svým přátelům, co {word} se vám líbí.",
       words: {
         music: "Hudba",
         videos: "Videa",
         streams: "Streamování",
         media: "Média"
       },
-      description: false,
+      description: "PreMiD je jednoduchý, výkonný nástroj, který vám umožňuje sdílet vaši aktuální aktivitu médií na více platformách jako YouTube, Disney+, Netflix a další. Zůstaňte ve spojení a nechte své přátele vidět, co děláte v reálném čase.",
       getStarted: "Jak Začít",
       sections: {
         feature: {
-          title: false,
+          title: "Proč se zamilujete PreMiD",
           feature1: {
-            title: false,
-            description: false
+            title: "Nastavení soukromí",
+            description: "Převeďte si nastavení ochrany osobních údajů a rozhodněte se, jaké aktivity sdílíte s ostatními. Vaše data, vaše pravidla."
           },
           feature2: {
-            title: false,
-            description: false
+            title: "Řízeno komunitou",
+            description: "Zkus bezpříkladnou podporu mnoha platforem, poháněných vášnivou a oddanou komunitou."
           },
           feature3: {
-            title: false,
-            description: false
+            title: "Přizpůsobitelné nastavení",
+            description: "Užívej zážitek z PreMiD s rozsáhlými možnostmi přizpůsobení tak, aby vyhovoval vašim preferencím a potřebám."
           },
           feature4: {
-            title: false,
-            description: false
+            title: "Snadné Nastavení",
+            description: "Zprovozněte PreMiD během chvilky. Náš jednoduchý proces nastavení zajišťuje bezproblémový start."
           },
           feature5: {
-            title: false,
-            description: false
+            title: "Vyhovující Discord ToS",
+            description: "Plně v souladu s podmínkami služby Discordu využíváním oficiálních koncových bodů poskytnutý Discordem."
           },
           feature6: {
-            title: false,
-            description: false
+            title: "Budoucí funkce",
+            description: "Zůstaňte naladěni pro vzrušující nové funkce a vylepšení, která ještě více zlepší váš zážitek z PreMiD."
           }
         },
         howItWorks: {
-          title: false,
+          title: "Jak to funguje",
           step1: {
-            title: false,
-            description: false
+            title: "Instalovat rozšíření",
+            description: "Přidat PreMiD do vašeho prohlížeče."
           },
           step2: {
-            title: false,
-            description: false
+            title: "Přihlašte se Discordem",
+            description: "Připojte PreMiD ke svému Discord účtu."
           },
           step3: {
-            title: false,
-            description: false
+            title: "Přidání aktivit",
+            description: "Vyberte si služby, které chcete zobrazit, jako YouTube, Disney+ a další."
           },
           step4: {
             title: "Užívej si",
-            description: false
+            description: "Sdílejte svou aktivitu a užijte si používání PreMiD."
           }
         },
         callToAction: {
-          title: false,
-          description: false,
+          title: "Připraveni začít?",
+          description: "Připojte se k {count} uživatelům, kteří již milují PreMiD.",
           button: "Přidej Se"
         }
       }
     },
     contributors: {
       title: "Přispěvatelé",
-      presenceDevelopers: false,
+      presenceDevelopers: "Vývojáři aktivit",
       staff: "Jadro týmu",
       supporters: "Podporovatelé",
       translators: "Překladatelé",
       avatar: {
-        tooltip: false
+        tooltip: "Kliknutím zkopírujete {name} avatar"
       }
     },
     downloads: {
-      title: "Stáhnout",
+      title: "Ke Stažení",
       steps: {
-        install: false,
-        login: false,
-        add: false,
+        install: "Instalovat rozšíření",
+        login: "Přihlašte se Discordem",
+        add: "Přidat aktivitu",
         showoff: "Předveď se!"
       },
       section: {
@@ -160,66 +160,66 @@ export default defineI18nLocale(() => ({
         }
       },
       browser: {
-        your: false,
-        other: false,
-        based: false
+        your: "Tvůj prohlížeč",
+        other: "Ostatní prohlížeče",
+        based: "Založeno na {browser}"
       },
       mobile: {
         title: "Špatné zprávy!",
-        description: false
+        description: "PreMiD není k dispozici pro mobilní zařízení, omlouváme se!"
       },
       alphaAccess: {
-        title: false,
-        description: false,
-        callToAction: false
+        title: "Odemkni exkluzivní přístup do alfa testování!",
+        description: "Krok do budoucnosti PreMiD tím, že se stane Patronem nebo sponzorem nás na GitHubu. Vaše podpora nejen podporuje náš vývoj, ale také vám poskytuje první přístup k nejinovativnějším funkcím, které vyrábíme. Zažijte špičku toho, co PreMiD může nabídnout a ovlivnit jeho trajektorii s vaší zpětnou vazbou. Nejde jen o to, abychom byli první. Je to o to, abychom byli součástí něčeho většího.",
+        callToAction: "Zjistěte více a zapojte se do inovací"
       },
-      faq: false,
+      faq: "Často kladené dotazy",
       faqs: {
         q1: {
-          question: false,
-          answer: false
+          question: "Co je PreMiD?",
+          answer: "PreMiD je jednoduchá nakonfigurovatelná aplikace, která dokáže v statusu na Discordu zobrazit, co zrovna děláš."
         },
         q2: {
-          question: false,
-          answer: false
+          question: "Jak mohu použít PreMiD?",
+          answer: "PreMiD můžeš použít instalací rozšíření a přihlášením pomocí tvého Discord účtu. Jakmile budeš přihlášen, můžeš přidat aktivitu do svého profilu a ukázat svým přátelům co právě děláš."
         },
         q3: {
-          question: false,
-          answer: false
+          question: "Je PreMiD proti Podmínkám Discordu?",
+          answer: "Ne, PreMiD není proti Podmínkám Discordu. PreMiD používá pro nastavení aktivity Discord API (včetně generovaných API koncových bodů poskytovaných Discordem). To znamená, že PreMiD je plně v souladu s Podmínkami Discordu."
         },
         q4: {
-          question: false,
-          answer: false
+          question: "Jaké služby podporuje PreMiD?",
+          answer: "PreMiD podporuje mnoho různých služeb, včetně YouTube, Twitch a Netflix. Seznam podporovaných služeb neustále roste. Celý seznam Presences můžete zobrazit na naší stránce obchodu."
         },
         q5: {
-          question: false,
-          answer: false
+          question: "Jak mohu přispět k PreMiD?",
+          answer: "Můžete přispět k PreMiD připojením k naší komunitě na GitHubu. Můžete pomoci hlášením problémů, navrhováním funkcí nebo přispěním kódu."
         },
         q6: {
-          question: false,
-          answer: false
+          question: "Je PreMiD zdarma?",
+          answer: "Ano, PreMiD je zdarma. My však přijímáme dary přes Patreon a GitHub Sponzorů na podporu rozvoj projektu."
         },
         q7: {
-          question: false,
-          answer: false
+          question: "Co mám dělat, když narazím na problém?",
+          answer: "Pokud narazíte na nějaké problémy, můžete se připojit na náš Discord server pro podporu. Máme také průvodce pro řešení problémů na naší dokumentaci."
         },
         q8: {
-          question: false,
-          answer: false
+          question: "PreMiD nepodporuje xyz, můžete ho přidat?",
+          answer: "Naše aktivity jsou řízeny komunitou, nemáme zdroje, které by přidávaly všechny platformy. Svou vlastní aktivitu však můžete přidat podle pokynů v naší dokumentaci."
         },
         q9: {
-          question: false,
-          answer: false
+          question: "Jak často je PreMiD aktualizován?",
+          answer: "Jsme malý dobrovolnický projekt, Naším cílem je aktualizovat PreMiD co nejčastěji, ale nemůžeme slíbit, že budeme vždy na vrcholu."
         }
       }
     },
     store: {
       title: "Obchod",
-      noPresence: false,
+      noPresence: "Hledání neodpovídá žádná aktivita...",
       presence: {
         button: {
-          reportIssue: false,
-          suggestFeature: false,
+          reportIssue: "Náhlasit chybu",
+          suggestFeature: "Navrhnout vylepšení",
           viewCode: "Zobrazit Kód"
         },
         title: {
@@ -227,11 +227,11 @@ export default defineI18nLocale(() => ({
           information: "Informace"
         },
         informationSection: {
-          contributors: false,
-          version: false,
-          users: false,
+          contributors: "Přispěvatelé:",
+          version: "Verze: {version}",
+          users: "Uživatelé: {users}",
           tags: "Štítky:",
-          supportedUrls: false
+          supportedUrls: "Podporované URL stránky:"
         }
       },
       header: {
@@ -245,7 +245,7 @@ export default defineI18nLocale(() => ({
     followUs: "Sleduj nás",
     supportUs: "Podpoř nás",
     more: "Více",
-    legal: false,
+    legal: "Právní dokumenty",
     supportList: {
       donate: "Podpořit",
       contribute: "Přispět",
@@ -258,25 +258,25 @@ export default defineI18nLocale(() => ({
     },
     legalList: {
       privacyPolicy: "Zásady ochrany osobních údajů",
-      termsOfService: false,
+      termsOfService: "Podmínky použití",
       cookiePolicy: "Zásady používání souborů cookie"
     },
     withLoveBy: "S",
     by: "od",
-    copyright: false
+    copyright: "©️ {year}-{currentYear} {company} Všechna práva vyhrazena."
   },
   error: {
     404: {
       title: false,
-      message: false
+      message: "Stránka, kterou hledáte, neexistuje."
     },
     500: {
       title: false,
-      message: false
+      message: "Na našem konci se něco pokazilo."
     },
     default: {
       title: "Chyba",
-      message: false,
+      message: "Na našem konci se něco pokazilo.",
       button: "Zpět"
     }
   }
